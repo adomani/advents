48f6cf04e7f0f655fb34ba47114b7bdee99ce36e
adomani <adomani@gmail.com>
2023-12-01 07:21:54 +0100

    Initial configuration

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «Advents»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package «advents» {
+  -- add package configuration options here
+}
+
+lean_lib «Advents» {
+  -- add library configuration options here
+}
+
+@[default_target]
+lean_exe «advents» {
+  root := `Main
+}
