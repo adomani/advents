ce0d290fd0cb66eed68dbf79a1f081f7805dd8e2
adomani <adomani@gmail.com>
2023-12-05 20:15:40 +0000

    Day 3: use array

--- Advents/day03.lean
+++ Advents/day03.lean
@@ -33,7 +33,7 @@ def nbs := Id.run do
 
 --#assert nbs == #[(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-def has_number_nb (d : List (List Char)) (lx ly : Int) (p : Int × Int) : Array (Nat × Nat) :=
+def has_number_nb (d : Array (Array Char)) (lx ly : Int) (p : Int × Int) : Array (Nat × Nat) :=
   Id.run do
     let mut cond := #[]
     for o in nbs do
@@ -45,20 +45,20 @@ def has_number_nb (d : List (List Char)) (lx ly : Int) (p : Int × Int) : Array 
           cond := cond.push (x, y)
     return cond
 
-def symb_pos (s : List (List Char)) : List (Int × Int) :=
+def symb_pos (s : Array (Array Char)) : Array (Int × Int) :=
   Id.run do
     let mut tot := #[]
-    for i in [:s.length] do
-      for j in [:s[0]!.length] do
+    for i in [:s.size] do
+      for j in [:s[0]!.size] do
         let c := s[i]![j]!
         if (! c.isDigit && ! c == '.') then
           tot := tot.push ((i, j) : Int × Int)
-    return tot.toList
+    return tot
 
 /-- get the coordinates of digits that are neighbours of symbols. -/
-def digs_in_nb (rows : List (List Char)) (symbs : List (Int × Int)) : Array (Nat × Nat) :=
-  let rx := rows.length
-  let ry := rows[0]!.length
+def digs_in_nb (rows : Array (Array Char)) (symbs : Array (Int × Int)) : Array (Nat × Nat) :=
+  let rx := rows.size
+  let ry := rows[0]!.size
   let tots := Id.run do
     let mut digs := #[]
     for s in symbs do
@@ -69,9 +69,9 @@ def digs_in_nb (rows : List (List Char)) (symbs : List (Int × Int)) : Array (Na
 
 /-- returns the natural number and the endpoints of the consecutive digits
 appearing in rows. -/
-def digs_ends (rows : List (List Char)) : Array (Nat × (Nat × Nat) × (Nat × Nat)) :=
-  let rx := rows.length
-  let ry := rows[0]!.length
+def digs_ends (rows : Array (Array Char)) : Array (Nat × (Nat × Nat) × (Nat × Nat)) :=
+  let rx := rows.size
+  let ry := rows[0]!.size
   Id.run do
     let mut tot := #[]
     for i in [:rx] do
@@ -91,7 +91,7 @@ def digs_ends (rows : List (List Char)) : Array (Nat × (Nat × Nat) × (Nat × 
           val := ""
     return tot
 
-def part1 (rows : List (List Char)) : Nat :=
+def part1 (rows : Array (Array Char)) : Nat :=
   let digs := digs_ends rows
   let inn_digs := digs_in_nb rows (symb_pos rows)
   Id.run do
@@ -105,23 +105,23 @@ def part1 (rows : List (List Char)) : Nat :=
   return tot
 
 #eval show MetaM _ from do
-  let answer := part1 ((← IO.FS.lines input).map String.toList).toList
+  let answer := part1 ((← IO.FS.lines input).map (List.toArray ∘ String.toList))
   IO.println (f!"Day 3, part 1: {answer}")
   guard (answer == 531932)
 
-def get_mul_pos (s : List (List Char)) : List (Int × Int) :=
+def get_mul_pos (s : Array (Array Char)) : Array (Int × Int) :=
   Id.run do
     let mut tot := #[]
-    for i in [:s.length] do
-      for j in [:s[0]!.length] do
+    for i in [:s.size] do
+      for j in [:s[0]!.size] do
         let c := s[i]![j]!
         if (c == '*') then
           tot := tot.push ((i, j) : Int × Int)
-    return tot.toList
+    return tot
 
-def get_num_nbs (rows : List (List Char)) (p : Int × Int) : Array Nat :=
+def get_num_nbs (rows : Array (Array Char)) (p : Int × Int) : Array Nat :=
   let digs := digs_ends rows
-  let inn_digs := digs_in_nb rows [p]
+  let inn_digs := digs_in_nb rows #[p]
   Id.run do
   let mut tot := #[]
   for d in digs do
@@ -132,7 +132,7 @@ def get_num_nbs (rows : List (List Char)) (p : Int × Int) : Array Nat :=
       tot := tot.push dig
   return tot
 
-def part2 (rows : List (List Char)) : Nat :=
+def part2 (rows : Array (Array Char)) : Nat :=
   let mul_pos := get_mul_pos rows
   let gearRatios := mul_pos.map fun p =>
     let digs := get_num_nbs rows p
@@ -142,8 +142,7 @@ def part2 (rows : List (List Char)) : Nat :=
   gearRatios.sum
 
 --#assert part2 ((test.splitOn "\n").map String.toList) == 467835
-
 #eval show MetaM _ from do
-  let answer := part2 ((← IO.FS.lines input).map String.toList).toList
+  let answer := part2 ((← IO.FS.lines input).map (List.toArray ∘ String.toList))
   IO.println (f!"Day 3, part 1: {answer}")
   guard (answer == 73646890)
