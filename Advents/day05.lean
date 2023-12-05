8d824c922984ccf7c22262e642dd98486be76f9f
adomani <adomani@gmail.com>
2023-12-05 19:14:42 +0000

    Day 5, Docs and cleanup

--- Advents/day05.lean
+++ Advents/day05.lean
@@ -10,6 +10,7 @@ def input : System.FilePath := "Advents/day05.input"
 
 --#eval do IO.println (← IO.FS.readFile input)
 
+/-- `test` is the test string for the problem. -/
 def test := "seeds: 79 14 55 13
 
 seed-to-soil map:
@@ -44,50 +45,11 @@ humidity-to-location map:
 60 56 37
 56 93 4"
 
-def List.toRanges (l : List Nat) : List (Nat × Nat) :=
-  match l with
-    | [dest, src, lth] => Id.run do
-      let mut lkup := #[]
-      for i in [:lth] do
-        lkup := lkup.push (src + i, dest + i)
-      return lkup.toList
-    | _ => dbg_trace f!"oh no! {l}"; []
+/-- `compact l` takes as input a list of lists and returns the list of lists of lists obtained
+from `l` by forming the list of sublists of consecutive elements of `l` that are non-empty.
 
-#eval IO.println <| ([[52, 50, 48], [50, 98, 2]].map List.toRanges).join
-
-def convert (n : Nat) (l : List (Nat × Nat)) : Nat :=
-  (l.lookup n).getD n
-
-#eval (List.iota 100).reverse.map (convert · ([[52, 50, 48], [50, 98, 2]].map List.toRanges).join)
-
-def conv (ins : List Nat) (n : Nat) : Nat :=
-  match ins with
-    | [dest, src, lth] => if src ≤ n && n < src + lth then dest + (n - src) else n
-    | _ => dbg_trace f!"oh no! {ins}"; 0
-
-def conv1 (inss : List (List Nat)) (n : Nat) : Nat :=
-  let ins := inss.find? fun ins => ins[1]! ≤ n && n < ins[1]! + ins[2]!
-  match ins with
-    | none     => n
-    | some ins => conv ins n
-
-#assert ([79, 14, 55, 13]).map (convert · ([[52, 50, 48], [50, 98, 2]].map .toRanges).join) == [81, 14, 57, 13]
-
-#eval (List.iota 100).reverse.map (convert · ([[52, 50, 48], [50, 98, 2]].map .toRanges).join) --== [81, 14, 57, 13]
-#eval (List.iota 100).reverse.map (conv [52, 50, 48] ∘ (conv [50, 98, 2])) --==
-#assert ([79, 14, 55, 13]).map (conv ([52, 50, 48])) == [81, 14, 57, 13]
---, [50, 98, 2]
-
-#eval
-  let l1 := (List.iota 100).reverse.map (convert · ([[52, 50, 48], [50, 98, 2]].map .toRanges).join)
---  let l2 := (List.iota 100).reverse.map (conv [52, 50, 48] ∘ (conv [50, 98, 2]))
-  let l2 := (List.iota 100).reverse.map (conv1 [[50, 98, 2], [52, 50, 48]])
-  let diffs := (List.iota 100).reverse.map fun i => if l1[i-1]! != l2[i-1]! then (l1[i-1]! , l2[i-1]!) else (0, 0)
-  diffs.filter (· != (0,0))
-
-
-#eval [[0], [], [], [1]].partition (List.length · != 0)
-
+`compact [[1, 2], [2], [], [], [2, 3], [4], []] = [[[1, 2], [2]], [[2, 3], [4]]]`
+-/
 partial
 def compact (l : List (List α)) : List (List (List α)) :=
   if l.length == 0 then [] else
@@ -95,63 +57,61 @@ def compact (l : List (List α)) : List (List (List α)) :=
   let fin := (l.dropWhile (List.length · != 0)).dropWhile (List.length · == 0)
   [init] ++ compact fin
 
-#eval compact [[], [0], [0], [], [], [0], [1]]
-#assert compact [[], [0], [0], [], [], [0], [1]] == [[], [[0], [0]], [[0], [1]]]
+--#assert compact [[], [0], [0], [], [], [0], [1]] == [[], [[0], [0]], [[0], [1]]]
 
-
-
---#check List.lookup
-#eval do
-  let maps := test
-  let nums := maps.splitOn "map"
-  let nums_and_empties := (nums.map (String.splitOn · "\n")).join.map (List.getNumbers ∘ String.toList)
-  IO.println <| compact nums_and_empties
-  let seeds := nums_and_empties[0]!
-  let instrs := (nums_and_empties.drop 1)
---  let nums := nums_and_empties.filter (List.length · != 0)
-  IO.println <| nums_and_empties
-  IO.println <| f!"\n{seeds}\n{instrs}"
-
-def get_seed_insts (maps : String) : List Nat × List (List (List Nat)) :=
+/-- `get_seed_instrs maps` takes as input the input of the problem.
+It returns a pair consisting of
+* the list of seeds of the problem;
+* the list of instructions for each conversion through which the seeds go.
+-/
+def get_seed_instrs (maps : String) : List Nat × List (List (List Nat)) :=
   let nums := maps.splitOn "map"
   let nums_and_empties := (nums.map (String.splitOn · "\n")).join.map (List.getNumbers ∘ String.toList)
   let cnums := compact nums_and_empties
   (cnums[0]![0]!, cnums.drop 1)
 
-def pass_through (instrs : List (List (List Nat))) (seeds : List Nat) : List Nat :=
+/-- `conv ins n` takes as input a list of natural numbers and a natural number.
+It returns the "elementary" transformation that `n` goes through when it finds the
+instructions `ins`.
+
+This is a helper function to `conv1` which in turn is a helper function to `pass_through`. -/
+def conv (ins : List Nat) (n : Nat) : Nat :=
+  match ins with
+    | [dest, src, lth] => if src ≤ n && n < src + lth then dest + (n - src) else n
+    | _ => dbg_trace f!"oh no! {ins}"; 0
+
+--#assert ([79, 14, 55, 13]).map (conv ([52, 50, 48])) == [81, 14, 57, 13]
+
+/-- `conv1 inss n` takes as input a list of lists of natural numbers and a natural number.
+It returns the transformation that `n` goes through following whichever one of the
+instructions in `inss` applies.  It uses `conv` for the "elementary transformation".
+
+This is a helper function to `pass_through`. -/
+def conv1 (inss : List (List Nat)) (n : Nat) : Nat :=
+  match inss.find? fun ins => ins[1]! ≤ n && n < ins[1]! + ins[2]! with
+    | none     => n
+    | some ins => conv ins n
+
+/-- `pass_through instrs seeds` takes as input the entries of the output of `get_seed_instrs`.
+It returns the value of the corresponding transformation on each element of `seeds`. -/
+def pass_through (seeds : List Nat) (instrs : List (List (List Nat))) : List Nat :=
   Id.run do
   let mut seeds := seeds
   for ins in instrs do
     seeds := seeds.map (conv1 ins)
   return seeds
+
+/-- `part1 maps` takes as input the input of the problem and returns the solution to part 1. -/
+def part1 (maps : String) : Nat :=
+  let (seeds, instrs) := get_seed_instrs maps
+  (pass_through seeds instrs).foldl min seeds[0]!
+
+--#assert part1 test == 35
+
 #eval show MetaM _ from do
-  let maps := test
-  let maps ← IO.FS.readFile input
-  let (seeds, instrs) := get_seed_insts maps
-  let seeds := pass_through instrs seeds
---  IO.println <| seeds
-  IO.println <| seeds.foldl min seeds[0]!
-  guard (seeds.foldl min seeds[0]! == 579439039)
---  IO.println <| nums_and_empties
---  IO.println <| f!"\n{seeds}\n{instrs}"
-
-
-#eval do
---  let maps ← IO.FS.readFile input
-  let maps := test
-  let nums := maps.splitOn "map"
-  let nums_and_empties := (nums.map (String.splitOn · "\n")).join.map (List.getNumbers ∘ String.toList)
-  let cnums := compact nums_and_empties
-  let mut seeds := cnums[0]![0]!
-  let instrs := (cnums.drop 1)  -- a list of lists of Nats
---  let instrs := (nums_and_empties.drop 1)
---  let nums := nums_and_empties.filter (List.length · != 0)
-  for ins in instrs do
-    let lkup := (ins.map List.toRanges).join
-    seeds := seeds.map (convert · lkup)
-  IO.println <| seeds.foldl min seeds[0]!
---  IO.println <| nums_and_empties
---  IO.println <| f!"\n{seeds}\n{instrs}"
+  let ans := part1 (← IO.FS.readFile input)
+  IO.println f!"Day 5, part1: {ans}"
+  guard (ans == 579439039)
 
 /-!
 #  Question 2
@@ -169,28 +129,29 @@ def rev_instrs (instrs : List (List (List Nat))) : List (List (List Nat)) :=
 #eval show MetaM _ from do
   let _maps := test
   let _maps ← IO.FS.readFile input
-  let (ini_seeds, instrs) := get_seed_insts _maps
-  let seeds := pass_through instrs ini_seeds
-  guard (pass_through (rev_instrs instrs) seeds == ini_seeds)
--/
+  let (ini_seeds, instrs) := get_seed_instrs _maps
+  let seeds := pass_through ini_seeds instrs
+  guard (pass_through seeds (rev_instrs instrs) == ini_seeds)
+--/
 
+/-- `part2 maps` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (maps : String) : Nat :=
   let fin_seeds := Id.run do
-    let (ini_seeds, instrs) := get_seed_insts maps
+    let (ini_seeds, instrs) := get_seed_instrs maps
     let mut breaks := []
     let mut currInst := []
     for ins in [:instrs.length] do
       let new := instrs[ins]!
       currInst := currInst ++ [new]
       let ends := new.map fun x => x[0]!
-      let begs := pass_through (rev_instrs currInst) ends
+      let begs := pass_through ends (rev_instrs currInst)
       breaks := breaks ++ begs
     let mut breaks_in_range := []
     for i in [:ini_seeds.length] do
       if i % 2 == 0 then
         breaks_in_range := ini_seeds[i]! :: breaks_in_range ++ breaks.filter fun x =>
           ini_seeds[i]! ≤ x && x < ini_seeds[i]! + ini_seeds[i+1]!
-    pass_through instrs breaks_in_range
+    pass_through breaks_in_range instrs
   (fin_seeds).foldl min fin_seeds[0]!
 
 --#assert part2 test == 46
