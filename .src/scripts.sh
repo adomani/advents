478c5769c0d4ba78e178dbc28b72d2f9e95b794e
adomani <adomani@gmail.com>
2023-12-07 17:51:19 +0100

    Add templates

--- .src/scripts.sh
+++ .src/scripts.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+getDay () {
+(
+  if [ -z "${1}" ]; then
+    ind="$( ls Advents/day*.input | tail -1 | sed 's=.*y[0]*\([0-9]*\).*=\1=' )"
+    ind=$((ind+1))
+  else ind="${1}"
+  fi
+  printf '%s' "${ind}"
+)
+}
+
 ##  `newday <n>` downloads the `day/n/input` from `Advent of Code`
 ##  and saves it to `Advents/i<0n>.txt`, where `<0n>` is a 2-digit
 ##  representation of `<n>`, padded with zeros.
@@ -7,14 +18,14 @@
 ##  choosing the smallest index larger than the largest file `Advents/i<val>.txt`.
 newday () {
   (
-    if [ -z "${1}" ]; then
-      ind="$( ls Advents/day*.input | tail -1 | sed 's=.*y[0]*\([0-9]*\).*=\1=' )"
-      ind=$((ind+1))
-    else ind="${1}"
-    fi
     croot;
+    ind="$( getDay "${1}" )"
     git switch master
-    wget "https://adventofcode.com/2023/day/${ind}/input" -O "Advents/day"$( printf '%02d' "${ind}")".input"
+    ind0="$( printf '%02d' "${ind}" )"
+    fname="Advents/day${ind0}"
+    touch "${fname}.input"
+    sed "s=<newDay>=${ind0}=" template.lean >> "${fname}.lean"
+#    wget "https://adventofcode.com/2023/day/${ind}/input" -O "Advents/day"$( printf '%02d' "${ind}")".input"
   )
 }
 
---
+++ template.lean
@@ -0,0 +1,34 @@
+import Advents.Utils
+open Lean
+
+/-- `input` is the location of the file with the data for the problem. -/
+def input : System.FilePath := "Advents/day<newDay>.input"
+
+/-!
+#  Question 1
+-/
+
+--#eval do IO.println (← IO.FS.readFile input)
+
+/-- `test` is the test string for the problem. -/
+def test := ""
+
+/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
+def part1 (dat : Array String) : Nat := sorry
+--def part1 (dat : String) : Nat := sorry
+
+--#assert part1 (test.splitOn "\n").toArray == ???
+
+--solve 1
+
+/-!
+#  Question 2
+-/
+
+/-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
+def part2 (dat : Array String) : Nat := sorry
+--def part2 (dat : String) : Nat :=
+
+--#assert part2 (test.splitOn "\n").toArray == ???
+
+--solve 2
