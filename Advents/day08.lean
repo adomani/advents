1226bb91bf659b0e25cae1ae56ec22c57f8de2d2
adomani <adomani@gmail.com>
2023-12-08 07:29:18 +0100

    Partial cleanup

--- Advents/day08.lean
+++ Advents/day08.lean
@@ -33,44 +33,11 @@ def getInstrMvs (s : String) : (List Char × (List (String × (String × String)
       (i.toList, part)
     | _ => dbg_trace "oh no!"; default
 
-#eval getInstrMvs test
-
 def mv (c : Char) (opt : String × String) : String :=
   match c with
     | 'L' => opt.1
     | 'R' => opt.2
     | _ => dbg_trace "mv: oh no!"; default
-
-def run (mvs : List Char) (lkup : (List (String × (String × String)))) : Nat :=
-  let fin := "ZZZ"
-  let lth := mvs.length
-  Id.run do
-    let mut init := "AAA"
-    let mut steps := 0
-    while init != fin do
-      let new := (lkup.lookup init).get!
-      init := mv (mvs[steps % lth]!) new
-      steps := steps + 1
---      dbg_trace (mvs[i % lth]!, new)
-    return steps
-
-/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
-def part1 (dat : String) : Nat :=
-  let (mvs, lkup) := getInstrMvs dat
-  run mvs lkup
-
---#assert part1 test == 2
-
-solve 1 14257 file
-
-#eval do
-  let (mvs, lkup) := getInstrMvs test
-  let (mvs, lkup) := getInstrMvs (← IO.FS.readFile input)
-  return run mvs lkup
-
-/-!
-#  Question 2
--/
 
 def runCond (first : String) (mvs : List Char) (lkup : (List (String × (String × String)))) (f : String → Bool) :
     Nat :=
@@ -84,43 +51,26 @@ def runCond (first : String) (mvs : List Char) (lkup : (List (String × (String 
       steps := steps + 1
     return steps
 
-#eval do
-  let (mvs, lkup) := getInstrMvs test
-  let (mvs, lkup) := getInstrMvs (← IO.FS.readFile input)
-  let endA := (lkup.filter fun x => String.endsWith (Prod.fst x) "A").map Prod.fst
-  let periods := endA.map (runCond · mvs lkup (String.endsWith · "Z"))
-  return periods.foldl Nat.lcm 1
+/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
+def part1 (dat : String) : Nat :=
+  let (mvs, lkup) := getInstrMvs dat
+  runCond "AAA" mvs lkup (· == "ZZZ")
 
+#assert part1 test == 2
 
+solve 1 14257 file
 
-#check String.endsWith
-def run2 (mvs : List Char) (lkup : (List (String × (String × String)))) : Nat :=
-  let fin := "ZZZ"
-  let lth := mvs.length
-  let endA := (lkup.filter fun x => x.1.endsWith "A").map Prod.fst
-  dbg_trace endA
-  default
---  Id.run do
---    let mut init := endA.map Prod.fst
---    let mut steps := 0
---    while init != fin do
---      let new := (lkup.lookup init).get!
---      init := mv (mvs[steps % lth]!) new
---      steps := steps + 1
-----      dbg_trace (mvs[i % lth]!, new)
---    return steps
-
-#eval do
-  let (mvs, lkup) := getInstrMvs test
-  let (mvs, lkup) := getInstrMvs (← IO.FS.readFile input)
-  return run2 mvs lkup
-
-
+/-!
+#  Question 2
+-/
 
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
-def part2 (dat : Array String) : Nat := sorry
---def part2 (dat : String) : Nat :=
+def part2 (dat : String) : Nat :=
+  let (mvs, lkup) := getInstrMvs dat
+  let endA := (lkup.filter fun x => String.endsWith (Prod.fst x) "A").map Prod.fst
+  let periods := endA.map (runCond · mvs lkup (String.endsWith · "Z"))
+  periods.foldl Nat.lcm 1
 
---#assert part2 (test.splitOn "\n").toArray == ???
+#assert part2 test = 2
 
---solve 2
+solve 2 16187743689077 file
