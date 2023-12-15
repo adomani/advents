62a9bb3e3f849a5a031bc31e653909d139d4ba56
Damiano Testa <adomani@gmail.com>
2023-12-15 09:36:19 +0100

    Part 2 clean up, slow

--- Advents/day15.lean
+++ Advents/day15.lean
@@ -54,85 +54,50 @@ solve 1 510013 file
 #  Question 2
 -/
 
-
-#eval do  -- 510013
---  IO.println dat.getLast!
-  let dat := atest
-  let dat := ((← IO.FS.readFile input).dropRight 1).splitOn "," |>.toArray
-  IO.println (← IO.FS.readFile input).back
-
-  IO.println (dat.map convert).sum
-  IO.println dat.size
---  for t in dat do IO.println (t, convert t)
-
-#eval Array.mkArray 256 (#[] : Array (Array String))
-
-#eval convert "qp"
-
 /-- returns the box number for the input string. -/
 def findBox (s : String) : Nat :=
-  let cv := if s.back == '-' then
+  if s.back == '-' then
     convert (s.dropRight 1)
   else
     convert (s.splitOn "=")[0]!
-  if 255 < cv then dbg_trace cv; cv else
-  cv
 
-#eval convert "rn"
+#assert convert "rn" == 0
 
 variable (dat : Array (Array String)) in
 /-- assumes that `s` ends with `-`, removes
 the first lens with label `s.dropRight 1`. -/
 def actOneSub (s : String) : Array (Array String) :=
   let lab := findBox s
---  dbg_trace s!"{lab}"
   let pos : Fin dat.size := ⟨lab % dat.size, Nat.mod_lt _ sorry⟩
---  dbg_trace s!"{dat[pos]}"
   let repl := (dat[pos].find? (String.isPrefixOf ((s.dropRight 1).push '='))).get!
---  dbg_trace s!"{repl}"
-  let dm := dat.set pos (dat[lab]!.erase repl)
-  dm
-
-#eval do
-  let init := #[ #[], #["pc"], #[], #["ciao", "pc=1", "d2", "pc=54"] ]
-  IO.println init
-  IO.println <| actOneSub init "pc-"
+  dat.set pos (dat[lab]!.erase repl)
 
 variable (dat : Array (Array String)) in
 /-- assumes that `s` contains `=`, replaces or adds
 the lens with the appropriate label in its relevan box. -/
 def actOneEq (s : String) : Array (Array String) :=
   let lab := findBox s
---  dbg_trace s!"{lab}"
   let pos : Fin dat.size := ⟨lab % dat.size, Nat.mod_lt _ sorry⟩
---  dbg_trace s!"{dat[pos]}"
   match (dat[pos].find? (String.isPrefixOf ((s.splitOn "=")[0]!.push '='))) with
     | none => dat.set pos (dat[lab]!.push s)
     | some repl =>
---      dbg_trace "found"
       dat.set pos (dat[lab]!.map fun x => if x == repl then s else x)
---      dat
---  dbg_trace s!"{repl}"
---  let dm := dat.set pos (dat[lab]!.erase repl)
---  dm
-#check Array.modify
-#eval do
-  let init := #[ #[], #["pc"], #[], #["ciao", "pc=1", "d2"] ]
-  IO.println init
-  let de := actOneSub init "pc-"
-  IO.println <| de
-  let de := actOneEq de "pc=12"
-  IO.println <| de
 
-def combineAux : Array (Array String) → List String → Array (Array String)
-  | dat, s::ss =>
-    let actOne := if s.back == '-' then actOneSub dat s else actOneEq dat s
-    combineAux (actOne) ss
-  | dat, [] => dat
-
+/-- `combine dat` takes as input the list of instructions and
+returns the final configuration of lenses in each box. -/
 def combine (dat : List String) : Array (Array String) :=
-  combineAux (Array.mkArray 256 (#[] : Array String)) dat
+  combineAux (Array.mkArray 256 (#[] : Array String)) dat where
+  /-- `combineAux` is an auxilliary function to `combine`. -/
+  combineAux : Array (Array String) → List String → Array (Array String)
+    | dat, s::ss =>
+      let actOne :=
+        if s.back == '-' then actOneSub dat s else actOneEq dat s
+      combineAux (actOne) ss
+    | dat, [] => dat
 
+/-- `focPowOne a` takes as input an array of strings and returns the
+focal power of that array.  The actual value will need to be multiplied
+by the position of the box containing `a` and then summed all up. -/
 def focPowOne (a : Array String) : Nat :=
   Id.run do
   let mut tot := 0
@@ -140,29 +105,12 @@ def focPowOne (a : Array String) : Nat :=
     tot := tot + (s + 1) * (a[s]!.toList).getNumbers[0]!
   return tot
 
-
-#eval do  -- 268497
-  let dat := (((← IO.FS.readFile input).dropRight 1).splitOn ",") --.take 100
-  let dat := atest.toList
-  let alls := (Array.range 256).zipWith ((combine dat).map focPowOne) (Nat.succ · * ·)
-  IO.println <| alls
-  IO.println <| alls.sum
-
---  too low:  267407
-#exit
-#eval do
---  IO.println dat.getLast!
---  let dat := ((← IO.FS.readFile input).dropRight 1).splitOn "," |>.toArray
-  let dat := atest
-  IO.println <| dat.map findBox
-
-
-
---  wrong: 509879
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
-def part2 (dat : Array String) : Nat := sorry
---def part2 (dat : String) : Nat :=
+def part2 (dat : String) : Nat :=
+  let dat := (if dat.back == '\n' then dat.dropRight 1 else dat).splitOn ","
+  let alls := (Array.range 256).zipWith ((combine dat).map focPowOne) (Nat.succ · * ·)
+  alls.sum
 
---#assert part2 (test.splitOn "\n").toArray == ???
+#assert part2 test == 145
 
---solve 2
+solve 2 268497 file
