ae38c22e4f858e6af945f23324c558a8e35effb9
adomani <adomani@gmail.com>
2023-12-21 10:05:59 +0100

    Setting up second approach for part 2.

--- Advents/day21.lean
+++ Advents/day21.lean
@@ -93,6 +93,68 @@ solve 1 3764
 #  Question 2
 -/
 
+namespace Part_2_brute
+
+/-- Similar to `mvs`, except that it reduces the coordinates modulo the size of the grid
+before checking if they belong to `rk`.
+
+This is good for a period configuration of rocks. -/
+def mvsmod (rk gd : HashSet pos) (sz : Nat) : HashSet pos :=
+  Id.run do
+  let mut new : HashSet pos := .empty
+  for g in gd do
+    let mvs := ([.U, .D, .L, .R].map (toPos · + g)).filter fun x => (rk.find? (x.1 % sz, x.2 % sz)).isNone
+    for m in mvs do
+      new := new.insert m
+  return new
+
+/-- Similar to `part1`, except that it uses `mvsmod` instead of `mvs`.
+Effectively, it reduces the coordinates modulo the size of the grid
+before checking if they correspond to a rock.
+
+This is good for a period configuration of rocks. -/
+def part1mod (dat : Array String) (sz : Nat) (n : Nat := 64) : Nat :=
+  let rk := getRocks dat
+  Id.run do
+  let mut gd : HashSet pos := HashSet.empty.insert (findS dat)
+  for _ in [:n] do
+    gd := (mvsmod rk gd sz)
+  return gd.size
+
+/-!
+Test that we can really solve the second part working modulo the size of the grid.
+-/
+/-
+#eval do show MetaM _ from do
+  let mut tots := #[]
+  for d in [6, 10, 50, 100] do
+    tots := tots.push <| part1mod atest atest.size d
+  IO.println <| tots
+  guard (tots == #[16, 50, 1594, 6536])
+--/
+
+end Part_2_brute
+
+/-!
+However, we are going to proceed by counting in bulk, by couting all "full" gardens at once
+and only resorting to evolving our moves for the fringes of the garden.
+-/
+
+/-- `onlyS dat` takes as input an array `dat` of strings and returns
+* `true` if the row and column containing `S` consist of a single `S` and the rest is `.`s;
+* `false` otherwise.
+
+It is `true` for the puzzle input, as you can see from the `#assert` line below.
+-/
+def onlyS (dat : Array String) : Bool :=
+  let (sx, sy) := findS dat
+  let s2 := [(sx, dat), (sy, dat.transpose)].map fun (c, t) =>
+    String.mk (t[c.toNat]!.toList.filter (! · == '.'))
+  s2 == ["S", "S"]
+
+#assert onlyS (← IO.FS.lines input)
+
+
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (dat : Array String) : Nat := sorry
 --def part2 (dat : String) : Nat :=
