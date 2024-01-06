f8ad5089b23c26557f35977ca3c2666fa46ceafa
adomani <adomani@gmail.com>
2024-01-06 08:52:30 +0100

    09: remove firstDiff by using List.sub, some syntactic changes

--- Advents/day09.lean
+++ Advents/day09.lean
@@ -19,26 +19,7 @@ def test := "0 3 6 9 12 15
 def atest := (test.splitOn "\n").toArray
 
 /-- `xbinom coeff degree n` equals `coeff * (n + degree` *choose* `degree)`. -/
-def xbinom (coeff : Int) (degree : Nat) (n : Nat) : Int := coeff * Nat.binom (n + degree) degree
-
-/-- `firstDiff l` takes an input a list `l` of integers and returns the list of first differences of `l`.
-The resulting list has length one less than the original one. -/
-def firstDiff (l : List Int) : List Int :=
-  (List.range (l.length - 1)).map fun i => (l[i+1]! - l[i]!)
-
-/-- `degCoeff l` takes an input a list `l` of integers and returns
-a degree `d` and a leading coefficient `c` such that `c * (x choose d)` is a polynomial
-whose values on `{ 0, .., l.length - 1 }` "approximate" `l`.
-In practice, it is found by computing iterated first differences and recording the last non-zero list. -/
-def degCoeff (l : List Int) : Nat × Int :=
-  if l.all (· == l.getD 0 0) then (0, 0) else
-  Id.run do
-  let mut vals := l
-  let mut lcs := #[l.getD 0 0]
-  while vals.any (· != 0) do
-    vals := firstDiff vals
-    lcs := lcs.push vals[0]!
-  return (lcs.size - 2, lcs.pop.back)
+def xbinom (coeff : Int) (degree n : Nat) : Int := coeff * Nat.binom (n + degree) degree
 
 /-- The difference of two lists of integers is simply the componentwise difference.
 The length of the result is the length of the shortest of the two lists. -/
@@ -48,14 +29,28 @@ instance : Sub (List Int) where
 #assert ([0, 1] - [4] : List Int) = [- 4]
 #assert ([0, 1] - [4, 1, 1] : List Int) = [- 4, 0]
 
+/-- `degCoeff l` takes an input a list `l` of integers and returns
+a degree `d` and a leading coefficient `c` such that `c * (x choose d)` is a polynomial
+whose values on `{ 0, ..., l.length - 1 }` "approximate" `l`.
+In practice, it is found by computing iterated first differences and recording the last non-zero list. -/
+def degCoeff (l : List Int) : Nat × Int :=
+  if l.all (· == l.getD 0 0) then (0, 0) else
+  Id.run do
+  let mut vals := l
+  let mut lcs := #[l.getD 0 0]
+  while vals.any (· != 0) do
+    vals := vals.drop 1 - vals  -- compute the first difference of the list `vals`
+    lcs := lcs.push vals[0]!
+  return (lcs.size - 2, lcs.pop.back)
+
 /-- `cs l` takes an input a list `l` of integers and iteratively returns coefficients and degrees of
 appearing in the "binomial" expansion re-creating the list `l` when evaluated on
-`{ 0, .., l.length - 1 }`. -/
+`{ 0, ..., l.length - 1 }`. -/
 partial
 def cs (l : List Int) : List (Nat × Int) :=
   let first := degCoeff l
   if first = default then default else
-  let new := cs (l - (List.range l.length).map fun x : Nat => xbinom first.2 first.1 x)
+  let new := cs (l - (List.range l.length).map (xbinom first.2 first.1 ·))
   first :: new
 
 /-- `toPol a n` takes as input an array of pairs `(degree, coeff)` and an integer `n`.
@@ -74,14 +69,14 @@ def parts (dat : Array (List Int)) : Int :=
     let di := dat[idx]!
     let dis := di.length
     let csi := (cs di).toArray
-    let corr := di[(dis - 1)]! - (toPol csi (dis - 1))
+    let corr := di[dis - 1]! - (toPol csi (dis - 1))
     let part := toPol csi dis
     tot := tot + part + corr
   return tot
 
 /-- `part1 oasis` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (oasis : Array String) : Int :=
-  parts <| oasis.map (String.getInts)
+  parts <| oasis.map String.getInts
 
 #assert part1 atest == 114
 
@@ -93,7 +88,7 @@ solve 1 1884768153
 
 /-- `part2 oasis` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (oasis : Array String) : Int :=
-  parts <| (oasis.map (String.getInts)).map .reverse
+  parts <| (oasis.map String.getInts).map .reverse
 
 #assert part2 atest == 2
 
