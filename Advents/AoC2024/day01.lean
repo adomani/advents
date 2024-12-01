1c935a406ea1d44f558a3b11321afe5c0da5a26c
adomani <adomani@gmail.com>
2024-12-01 06:04:04 +0100

    input and template

---
+++ Advents/AoC2024/day01.lean
@@ -0,0 +1,46 @@
+import Advents.Utils
+open Lean
+
+namespace Day01
+
+/-- `input` is the location of the file with the data for the problem. -/
+def input : System.FilePath := "Advents/AoC2024/day01.input"
+
+/-!
+#  Question 1
+-/
+
+--#eval do IO.println (← IO.FS.readFile input)
+
+/-- `test` is the test string for the problem. -/
+def test := "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+
+/-- `atest` is the test string for the problem, split into rows. -/
+def atest := (test.splitOn "\n").toArray
+
+/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
+def part1 (dat : Array String) : Nat := sorry
+--def part1 (dat : String) : Nat := sorry
+
+--#assert part1 atest == ???
+
+--solve 1
+
+/-!
+#  Question 2
+-/
+
+/-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
+def part2 (dat : Array String) : Nat := sorry
+--def part2 (dat : String) : Nat :=
+
+--#assert part2 atest == ???
+
+--solve 2
+
+end Day01
