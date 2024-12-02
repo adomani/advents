e7260b06cd377f59705fcaf646b870e370e523c1
Damiano Testa <adomani@gmail.com>
2024-12-02 14:18:50 +0000

    clean up and docs

--- Advents/AoC2021/day09.lean
+++ Advents/AoC2021/day09.lean
@@ -22,26 +22,16 @@ def test := "2199943210
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
-#eval do
-  let dat := atest
-  let dat ← IO.FS.lines input
-  --let mut lows := 0
+/--
+`lowPoints dat` takes as input the array of strings that are the input to the problem.
+It computes
+* the `HashSet` of the locations of the "low points" -- these are the points on the grid
+  that have value less than or equal to all of their neighbours *and* actually strictly
+  smaller than some neighbour (useful for part 2);
+* the sum of the risk levels (useful for part 1).
+-/
+def lowPoints (dat : Array String) : Std.HashSet (Nat × Nat) × Nat := Id.run do
   let mut heights := 0
-  for d in [0:dat.size] do
-    let prev := dat[d-1]!
-    let next := dat[d+1]?.getD dat[d]!
-    let row := dat[d]!
-    for c in [0:row.length] do
-      let curr := row.get ⟨c⟩
-      let rownext := if c == row.length - 1 then curr else row.get ⟨c+1⟩
-      let rowprev := if c == 0              then curr else row.get ⟨c-1⟩
-      if (curr ≤ rownext && curr ≤ rowprev && curr ≤ prev.get ⟨c⟩ && curr ≤ next.get ⟨c⟩) &&
-        ! (curr == rownext && curr == rowprev && curr == prev.get ⟨c⟩ && curr == next.get ⟨c⟩)
-      then
-        heights := heights + 1 + ("".push curr).toNat!
-  IO.println heights
-
-def lowPoints (dat : Array String) : Std.HashSet (Nat × Nat) := Id.run do
   let mut lowPoints : Std.HashSet (Nat × Nat) := {}
   for d in [0:dat.size] do
     let prev := dat[d-1]!
@@ -55,26 +45,11 @@ def lowPoints (dat : Array String) : Std.HashSet (Nat × Nat) := Id.run do
         ! (curr == rownext && curr == rowprev && curr == prev.get ⟨c⟩ && curr == next.get ⟨c⟩)
       then
         lowPoints := lowPoints.insert (d, c)
-  lowPoints
-
-
+        heights := heights + 1 + ("".push curr).toNat!
+  (lowPoints, heights)
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
-def part1 (dat : Array String) : Nat := Id.run do
-  let mut heights := 0
-  for d in [0:dat.size] do
-    let prev := dat[d-1]!
-    let next := dat[d+1]?.getD dat[d]!
-    let row := dat[d]!
-    for c in [0:row.length] do
-      let curr := row.get ⟨c⟩
-      let rownext := if c == row.length - 1 then curr else row.get ⟨c+1⟩
-      let rowprev := if c == 0              then curr else row.get ⟨c-1⟩
-      if (curr ≤ rownext && curr ≤ rowprev && curr ≤ prev.get ⟨c⟩ && curr ≤ next.get ⟨c⟩) &&
-        ! (curr == rownext && curr == rowprev && curr == prev.get ⟨c⟩ && curr == next.get ⟨c⟩)
-      then
-        heights := heights + 1 + ("".push curr).toNat!
-  heights
+def part1 (dat : Array String) : Nat := (lowPoints dat).2
 
 #assert part1 atest == 15
 
@@ -84,11 +59,21 @@ solve 1 603
 #  Question 2
 -/
 
+/--
+`ExpandingDomain` is a structure to help "growing" from a "low point" to a whole basin.
+It represents a potentially intermediate situation, where
+* `visited` are the positions that have already been verified to be inside the basin and
+* `front` which are the positions that were last added to `visited`.
+
+Cf `growOnce` and `grow`: at each iteration, from each entry in `front`,
+they scan the neighbours and expand `visited`
+-/
 structure ExpandingDomain where
   visited : Std.HashSet (Nat × Nat)
   front : Std.HashSet (Nat × Nat)
   deriving Inhabited
 
+/-- Expands an `ExpandingDomain` once, using the reference `grid`. -/
 def growOnce (domain : ExpandingDomain) (grid : Array String) : ExpandingDomain := Id.run do
   let mut frt := {}
   let mut vis := domain.visited
@@ -106,34 +91,16 @@ def growOnce (domain : ExpandingDomain) (grid : Array String) : ExpandingDomain 
       frt := frt.insert q
   return { visited := vis, front := frt }
 
+/-- Expands an `ExpandingDomain` until it no longer grows. -/
 partial
-def grow (domain : ExpandingDomain) (grid : Array String) : ExpandingDomain := Id.run do
-  let mut vis := domain.visited
+def grow (domain : ExpandingDomain) (grid : Array String) : ExpandingDomain :=
+  let vis := domain.visited
   let new := growOnce domain grid
   if new.visited == vis then new else grow new grid
 
-#eval do
-  let dat := atest
-  let dat ← IO.FS.lines input
-  let roots := lowPoints dat
-  let mut basins := #[]
-  for r in roots do
-    let seed := {r}
-    let gr := grow {visited := seed, front := seed} dat
-    let vis := gr.visited
-    --let picture := toPic (vis.toArray.map (fun a : Nat × Nat => (a.1, a.2))) 100 100
-    basins := basins.push vis.size
-    --dbg_trace s!"{vis.size}"
-    --draw picture
-  let sortedBasins := basins.qsort (· > ·)
-  IO.println <| sortedBasins
-  IO.println <| (sortedBasins.take 3).prod
-  --let _ ← draws picture
-  --IO.println <| draw <| toPic (gr.visited.toArray.map (fun a => (a.1, a.2))) 10 10
-
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (dat : Array String) : Nat := Id.run do
-  let roots := lowPoints dat
+  let (roots, _) := lowPoints dat
   let mut basins := #[]
   for r in roots do
     let seed := {r}
