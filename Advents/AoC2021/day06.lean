78fde53e0944c66ba1e481b93d368b16a00eeda2
adomani <adomani@gmail.com>
2024-12-02 06:02:57 +0000

    shorten

--- Advents/AoC2021/day06.lean
+++ Advents/AoC2021/day06.lean
@@ -13,25 +13,27 @@ def input : System.FilePath := "Advents/AoC2021/day06.input"
 /-- `test` is the test string for the problem. -/
 def test := "3,4,3,1,2"
 
-/-- Convert an array of lanterfish to what it becomes the following day. -/
-def step (s : Array Nat) : Array Nat := Id.run do
-  let mut t := s
-  for i in [0:s.size] do
-    if s[i]! == 0 then
-      t := (t.modify i (fun _ => 6)).push 8
-    else
-      t := t.modify i (· - 1)
-  return t
+/-- Convert an array of tallied lanterfish to what it becomes the following day. -/
+def shift (as : Array Nat) : Array Nat :=
+  let first := as[0]!
+  let rest := (as.erase first).push first
+  rest.modify 6 (· + first)
 
-/-- Convert an array of lanterfish to what it becomes after a given number of days. -/
-def stepMany (s : Array Nat) : Nat → Array Nat
-  | 0 => s
-  | n + 1 => step (stepMany s n)
+/-- Convert an array of tallied lanterfish to what it becomes after a given number of days. -/
+def shiftMany (as : Array Nat) : Nat → Array Nat
+  | 0 => as
+  | n + 1 => shiftMany (shift as) n
+
+/--
+Convert a list `as` of lanternfish and a `n` number of days to
+the number of lanternfish that are present after `n` days.
+-/
+def tallyAndCountAfter (as : List Nat) (n : Nat) : Nat :=
+  let dat := (Array.range 9).map as.count
+  (shiftMany dat n).sum
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
-def part1 (dat : String) : Nat :=
-  let dat := dat.getNats.toArray
-  (stepMany dat 80).size
+def part1 (dat : String) : Nat := tallyAndCountAfter dat.getNats 80
 
 #assert part1 test == 5934
 
@@ -41,23 +43,8 @@ solve 1 380758 file
 #  Question 2
 -/
 
-def shift (as : Array Nat) : Array Nat :=
-  let first := as[0]!
-  let rest := (as.erase first).push first
-  rest.modify 6 (· + first)
-
-def shiftMany (as : Array Nat) : Nat → Array Nat
-  | 0 => as
-  | n + 1 => shift (shiftMany as n)
-
-#eval
-  let dat := (Array.range 9).map test.getNats.count
-  ((shiftMany dat 256).sum, (shiftMany dat 256))
-
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
-def part2 (dat : String) : Nat :=
-  let dat := (Array.range 9).map dat.getNats.count
-  (shiftMany dat 256).sum
+def part2 (dat : String) : Nat := tallyAndCountAfter dat.getNats 256
 
 #assert part2 test == 26984457539
 
