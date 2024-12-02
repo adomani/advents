be78b7dd5d4e45bae9a5323d43210fc1e85cafae
adomani <adomani@gmail.com>
2024-12-02 21:55:33 +0000

    docs and merge

--- Advents/AoC2021/day10.lean
+++ Advents/AoC2021/day10.lean
@@ -27,18 +27,15 @@ def test := "[({(<(())[]>[[{[]{<()<>>
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
+/-- If the input character is a closed parenthesis, return the corresponding open parenthesis, otherwise return `A`. -/
+def close : Char → Char
+  | ')' => '('
+  | ']' => '['
+  | '}' => '{'
+  | '>' => '<'
+  | _ => 'A'
 
-def isOpen : Char → Bool
-  | '(' | '[' | '{' | '<' => true
-  | _ => false
-
-def close : Char → Option Char
-  | ')' => some '('
-  | ']' => some '['
-  | '}' => some '{'
-  | '>' => some '<'
-  | _ => none
-
+/-- If the input character is an open parenthesis, return the corresponding closed parenthesis, otherwise return `A`. -/
 def openC : Char → Char
   | '(' => ')'
   | '[' => ']'
@@ -46,6 +43,7 @@ def openC : Char → Char
   | '<' => '>'
   | _ => 'A'
 
+/-- The illegal character penalty for each character. -/
 def points : Char → Nat
   | ')' => 3
   | ']' => 57
@@ -53,43 +51,35 @@ def points : Char → Nat
   | '>' => 25137
   | _ => 0
 
-#eval do
-  let mut pts := 0
-  let dat := atest
-  let dat ← IO.FS.lines input
+/--
+For each input string, compute whether the string has malformed parentheses or is well-formed, but missing some
+trailing closed parentheses.
+In the former case, append the total penalty to the first output array.
+In the latter case, append the list of missing closed parentheses to complete a correct syntax.
+-/
+def errorsOrClosing (dat : Array String) : Array Nat × Array (List Char) :=
+ Id.run do
+  let mut pts := #[]
+  let mut closeParens := #[]
   for lin in dat do
-  --let lin := atest[2]!
     let mut queue := #[]
     let mut err := #[]
     for c in lin.toList do
-      if isOpen c then
+      if openC c != 'A' then
         queue := queue.push c
-      else if some queue.back? == close c
+      else if queue.back? == close c
       then
         queue := queue.pop
       else
         err := err.push [queue.back?.map openC, some c].reduceOption
-    match err[0]? with
-      | none => continue --IO.println "No error found."
-      | some e => pts := pts + points e[1]!; --IO.println s!"Expected '{e[0]!}', found '{e[1]!}'"
-  IO.println s!"Total points: {pts}"
+    if let some e := err[0]? then pts := pts.push <| points e[1]!
+    else
+      let closes := queue.reverse.map openC
+      closeParens := closeParens.push closes.toList
+  return (pts, closeParens)
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
-def part1 (dat : Array String) : Nat := Id.run do
-  let mut pts := 0
-  for lin in dat do
-    let mut queue := #[]
-    let mut err := #[]
-    for c in lin.toList do
-      if isOpen c then
-        queue := queue.push c
-      else if some queue.back? == close c
-      then
-        queue := queue.pop
-      else
-        err := err.push [queue.back?.map openC, some c].reduceOption
-    if let some e := err[0]? then pts := pts + points e[1]!
-  pts
+def part1 (dat : Array String) : Nat := (errorsOrClosing dat).1.sum
 
 #assert part1 atest == 26397
 
@@ -99,7 +89,7 @@ solve 1 167379
 #  Question 2
 -/
 
-
+/-- The closing character value of each character. -/
 def pointsC : Char → Nat
   | ')' => 1
   | ']' => 2
@@ -107,55 +97,17 @@ def pointsC : Char → Nat
   | '>' => 4
   | _ => 0
 
+/-- The total score of a list of closing parentheses. -/
 def evalCompletion (l : List Char) : Nat := Id.run do
   let mut score := 0
   for c in l do
     score := 5 * score + pointsC c
   return score
 
-#eval do
-  let dat := atest
-  let dat ← IO.FS.lines input
-  let mut pts := #[]
-  for lin in dat do
-  --let lin := atest[2]!
-    let mut queue := #[]
-    let mut err := #[]
-    for c in lin.toList do
-      if isOpen c then
-        queue := queue.push c
-      else if some queue.back? == close c
-      then
-        queue := queue.pop
-      else
-        err := err.push [queue.back?.map openC, some c].reduceOption
-    if err[0]?.isNone then
-      let closes := queue.reverse.map openC
-      pts := pts.push closes.toList
-      --IO.println s!"{closes}"
-  let scores := pts.map evalCompletion
-  IO.println s!"Total points: {scores}"
-  let sorted := scores.qsort (· < ·)
-  IO.println sorted[sorted.size/2]!
-
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
-def part2 (dat : Array String) : Nat := Id.run do
-  let mut pts := #[]
-  for lin in dat do
-    let mut queue := #[]
-    let mut err := #[]
-    for c in lin.toList do
-      if isOpen c then
-        queue := queue.push c
-      else if some queue.back? == close c
-      then
-        queue := queue.pop
-      else
-        err := err.push [queue.back?.map openC, some c].reduceOption
-    if err[0]?.isNone then
-      let closes := queue.reverse.map openC
-      pts := pts.push closes.toList
-  let scores := pts.map evalCompletion
+def part2 (dat : Array String) : Nat :=
+  let (_, npts) := errorsOrClosing dat
+  let scores := npts.map evalCompletion
   let sorted := scores.qsort (· < ·)
   sorted[sorted.size / 2]!
 
