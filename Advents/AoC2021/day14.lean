9bfd7a3b4322b3c647d177e00bfc19aa344b981c
adomani <adomani@gmail.com>
2024-12-03 21:23:53 +0000

    part 2

--- Advents/AoC2021/day14.lean
+++ Advents/AoC2021/day14.lean
@@ -87,20 +87,73 @@ solve 1 2233
 #  Question 2
 -/
 
+structure SplitPoly where
+  rules : Std.HashMap (Char × Char) Char
+  poly  : Std.HashMap (Char × Char) Nat
+  ends  : Char × Char
+
+def mkSplitPoly (dat : Array String) : SplitPoly where
+  rules := Id.run do
+    let mut h := {}
+    for d in dat do
+      if let [a, b] := d.splitOn " -> " then
+        h := h.insert (a.get 0, a.get ⟨1⟩) (b.get 0)
+    return h
+  poly  := Id.run do
+    let mut h := {}
+    let pol := dat[0]!
+    for i in [0:pol.length - 1] do
+      let new := (pol.get ⟨i⟩, pol.get ⟨i + 1⟩)
+      let fd := h.getD new 0
+      h := h.insert new (fd + 1)
+    return h
+  ends := (dat[0]!.get 0, dat[0]!.back)
+
+def insertSplitOnce (p : SplitPoly) : SplitPoly where
+  rules := p.rules
+  poly := Id.run do
+    let mut new := {}
+    for (i@(l, r), val) in p.poly do
+      match p.rules.get? i with
+        | none => dbg_trace "here '{i}'"; new := new.insert i val
+        | some mid =>
+          let lval := new.getD (l, mid) 0
+          new := new.insert (l, mid) (lval + val)
+          let rval := new.getD (mid, r) 0
+          new := new.insert (mid, r) (rval + val)
+    return new
+  ends := p.ends
+
+def insertSplit (p : SplitPoly) : Nat → SplitPoly
+  | 0 => p
+  | n + 1 => insertSplit (insertSplitOnce p) n
+
+def countSplit (p : SplitPoly) : Std.HashMap Char Nat := Id.run do
+  let mut h := {}
+  for ((l, _r), val) in p.poly do
+    let acc := h.getD l 0
+    h := h.insert l (acc + val)
+  let acc := h.getD p.ends.2 0
+  h := h.insert p.ends.2 (acc + 1)
+  return h
+
 #eval do
   let dat := atest
   let dat ← IO.FS.lines input
-  let p := mkPolymer dat
-  let p := insert p 10
-  let vals := (tally p.poly).toArray.qsort (·.2 < ·.2)
-  IO.println (vals.back!.2 - vals[0]!.2)
+  let ps := mkSplitPoly dat
+  let p' := insertSplit ps 40
+  let fin := (countSplit p').toArray.qsort (·.2 < ·.2)
+  IO.println (fin.back!.2 - fin[0]!.2, fin)
 
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
-def part2 (dat : Array String) : Nat := sorry
---def part2 (dat : String) : Nat :=
+def part2 (dat : Array String) : Nat :=
+  let ps := mkSplitPoly dat
+  let p' := insertSplit ps 40
+  let fin := (countSplit p').toArray.qsort (·.2 < ·.2)
+  fin.back!.2 - fin[0]!.2
 
---#assert part2 atest == ???
+#assert part2 atest == 2188189693529
 
---solve 2
+solve 2 2884513602164
 
 end Day14
