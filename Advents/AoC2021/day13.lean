4267ffc21cf1d3811ae0bcf5368c1f3780f870ff
Damiano Testa <adomani@gmail.com>
2024-12-03 14:51:59 +0000

    partial part 1

--- Advents/AoC2021/day13.lean
+++ Advents/AoC2021/day13.lean
@@ -38,6 +38,57 @@ fold along x=5"
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
+
+def getGrid (inp : Array String) : Std.HashSet pos := Id.run do
+  let mut h := {}
+  for l in inp do
+    if let [x, y] := l.getInts then
+      h := h.insert (x, y)
+  return h
+
+def foldX (n : Int) (g : Std.HashSet pos) : Std.HashSet pos := Id.run do
+  let mut h := {}
+  for p in g do
+    h := h.insert (if p.1 < n then p else (2 * n - p.1, p.2))
+  return h
+
+def foldY (n : Int) (g : Std.HashSet pos) : Std.HashSet pos := Id.run do
+  let mut h := {}
+  for p in g do
+    h := h.insert (if p.2 < n then p else (p.1, 2 * n - p.2))
+  return h
+
+/-- A function to draw the `state` of the `OctoState` -- just needed for pretty pictures, no actual content. -/
+def drawHash (h : Std.HashSet pos) (Nx Ny : Nat) : Array String := Id.run do
+  let mut fin := #[]
+  for i in [0:Nx] do
+    let mut str := ""
+    for j in [0:Ny] do
+      match h.get? (i, j) with
+        | some d => str := str ++ "#"
+        | none => str := str.push '·'
+    fin := fin.push str
+  return fin
+
+
+#eval
+  let dat := atest
+  let grid := getGrid dat
+  let fx := foldY 7 grid
+  let fx := foldX 5 fx
+  dbg_trace fx.size
+  draws <| drawHash fx 10 20
+  --dat
+
+#eval do
+  let mut val := (655, 447)
+  IO.println val
+  for _ in (List.range 6) do
+    val := (val.1 / 2, val.2 / 2)
+    IO.println val
+
+
+
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := sorry
 --def part1 (dat : String) : Nat := sorry
