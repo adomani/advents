6dac9e9152297430bc919f6a09ecf562a1e3a85d
Damiano Testa <adomani@gmail.com>
2024-12-03 13:55:15 +0000

    prune code

--- Advents/AoC2021/day12.lean
+++ Advents/AoC2021/day12.lean
@@ -10,8 +10,6 @@ def input : System.FilePath := "Advents/AoC2021/day12.input"
 #  Question 1
 -/
 
---#eval do IO.println (← IO.FS.readFile input)
-
 /-- `test` is the test string for the problem. -/
 def test := "start-A
 start-b
