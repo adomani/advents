15b59f714c360d68234e73cb42521ab7ff249b30
adomani <adomani@gmail.com>
2024-12-04 23:19:25 +0000

    more experiments

--- Advents/AoC2021/day16.lean
+++ Advents/AoC2021/day16.lean
@@ -62,8 +62,8 @@ structure Packet where
   ID : Nat
   lth : Option (Nat × Nat)
   lit : Option Nat := none
-  --ps : Array Packet
-  deriving Inhabited
+  ps : Array Packet
+  deriving Inhabited, BEq
 
 def typeID : Nat → String
   | 0 => "sum"
@@ -76,13 +76,16 @@ def typeID : Nat → String
   | 7 => "equal to"
   | _ => "not a valid ID"
 
-instance : ToString Packet where
-  toString p :=
+partial
+def toString (p : Packet) : String :=
     let lID := match p.lth with
       | none => s!"literal: '{p.lit.getD 0}'"
       | some (0, lth) => s!"length ID: subpackets of total length {lth}"
       | some (_, lth) => s!"length ID: {lth} subpackets"
-    s!"v:  {p.version}\nID: {p.ID} '{typeID p.ID}' \n{lID}" --\npackets: {p.ps.size}"
+    s!"v:  {p.version}\nID: {p.ID} '{typeID p.ID}' \n{lID}\npackets: {p.ps.map toString}"
+
+instance : ToString Packet where
+  toString := toString
 
 def _root_.String.read (s : String) (n : Nat) : Nat × String :=
   (bitToNat <| s.take n, s.drop n)
@@ -108,7 +111,31 @@ def decodeOne (s : String) : Packet × String :=
       let tk := if ltype == 0 then 15 else 11
       let (len, s) := s.read tk
       (some (ltype, len), none, s)
-  ({version := ver, ID := ID, lth := lth, lit := lit}, s)
+  ({version := ver, ID := ID, lth := lth, lit := lit, ps := #[]}, s)
+
+partial
+def decodeNest (s : String) : Packet :=
+  if s.all (· == '0') then default
+  else
+  let (p, s) := decodeOne s
+  match p.lth with
+    | none =>
+      dbg_trace "nesting in literal: {s}"
+      {p with ps := #[decodeNest s]}
+    | some (0, l) =>
+      let nest := decodeNest (s.take l)
+      let cand := {p with ps := #[nest]}
+      let next := decodeNest (s.drop l)
+      if next == default then cand else {cand with ps := cand.ps.push next}
+    | some (_, l) => Id.run do
+      let mut (q, t) := (#[], s)
+      for _ in [0:l] do
+        let (np, ns) := decodeOne s
+        t := ns
+        q := q.push np
+      let cand := {p with ps := q}
+      let next := decodeNest t
+      if next == default then return cand else return {cand with ps := cand.ps.push next}
 
 partial
 def decodeAll (s : String) : Array Packet :=
@@ -117,20 +144,29 @@ def decodeAll (s : String) : Array Packet :=
     let (p, s) := decodeOne s
     #[p] ++ decodeAll s
 
-#eval do
-  let inp := "00111000000000000110111101000101001010010001001000000000"
-  let inp := "11101110000000001101010000001100100000100011000001100000"
-  let inp := hexToString "8A004A801A8002F478"
-  let inp := hexToString "620080001611562C8802118E34"
-  let inp := hexToString "C0015000016115A2E0802F182340"
-  let inp := hexToString "A0016C880162017C3686B18A3D4780"
-  let inp := hexToString (← IO.FS.readFile input)
-  let ds := decodeAll inp
+def printDecs (s : String) (nest? : Bool := true) : IO Unit := do
+  let ds := if nest? then #[decodeNest s] else decodeAll s
   let mut vers := 0
   for d in ds do
     vers := vers + d.version
     IO.println s!"\n{d}"
   IO.println s!"\n---\n\nTotal version: {vers}"
+
+
+
+#eval do
+  let inp := "00111000000000000110111101000101001010010001001000000000"
+  let inp := "11101110000000001101010000001100100000100011000001100000"
+  let inp := hexToString "8A004A801A8002F478"
+  let inp := hexToString "620080001611562C8802118E34"
+  let inp := hexToString "A0016C880162017C3686B18A3D4780"
+  let inp := hexToString "C0015000016115A2E0802F182340"
+  let inp := hexToString (← IO.FS.readFile input)
+  --let dn := decodeNest inp
+  --let dn := dn[1]!
+  --IO.println s!"dn: {dn}"
+  printDecs inp
+  printDecs inp false
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : String) : Nat := Id.run do
