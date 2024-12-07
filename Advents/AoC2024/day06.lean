3182a2f3f97d1b742f42ee562e022a404e1372f1
adomani <adomani@gmail.com>
2024-12-07 19:14:40 +0100

    docs

--- Advents/AoC2024/day06.lean
+++ Advents/AoC2024/day06.lean
@@ -27,8 +27,18 @@ def test := "....#.....
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
+/-- Rotation by 90⁰ clockwise. -/
 def rot (p : pos) : pos := (p.2, - p.1)
 
+/--
+The state for the grid.
+* `mz` is the location of the obstacles.
+* `S` is the current location of the guard.
+* `d` is the current direction of the guard.
+* `bd` is the "boundary": it is the location of every point in the grid and it is used to find
+  out when the guard leaves it.
+* `loop` records whether the guard is known to be in a loop.
+-/
 structure GuardMoves where
   mz : Std.HashSet pos
   S  : pos
@@ -38,6 +48,7 @@ structure GuardMoves where
   loop : Bool := false
   deriving Inhabited
 
+/-- Creates the initial state of the maze, with the given input. -/
 def mkGuardMoves (dat : Array String) : GuardMoves where
   mz := sparseGrid dat (· == '#')
   S  := (sparseGrid dat (· == '^')).toArray[0]!
@@ -45,6 +56,7 @@ def mkGuardMoves (dat : Array String) : GuardMoves where
   visited := (sparseGrid dat (· == '^')).fold (init := {}) (·.insert <| Prod.mk · (-1, 0))
   bd := sparseGrid dat (fun _ => true)
 
+/-- Updates the `GuardMoves` state, by taking one more step (which could be just to rotate). -/
 def move (gm : GuardMoves) : GuardMoves :=
   let candPos := (gm.S + gm.d, gm.d)
   if gm.visited.contains candPos then {gm with loop := true} else
@@ -55,11 +67,15 @@ def move (gm : GuardMoves) : GuardMoves :=
   else
     {gm with d := rot gm.d}
 
+/-- Updates the `GuardMoves` state, by taking `n` steps -- the iteration of `move`. -/
 def moveN (gm : GuardMoves) : Nat → Std.HashSet pos × Bool
   | 0 => (gm.visited.fold (init := {}) (·.insert ·.1), gm.loop)
   | n + 1 => moveN (move gm) n
 
-
+/--
+Repeatedly take one more step until either the guard exits the grid, or it repeats a
+position-direction pair.
+-/
 def moveUntil (gm : GuardMoves) : Std.HashSet pos × Bool := Id.run do
   let mut gm := gm
   while gm.d != (0, 0) && !gm.loop do
@@ -187,7 +203,6 @@ def part2 (dat : Array String) : Nat := Id.run do
   obsts.size
 
 #assert part2 atest == 6
-set_option trace.profiler true in
-solve 2 1770
+--solve 2 1770
 
 end Day06
