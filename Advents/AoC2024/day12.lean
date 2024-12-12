c53a421b2103be30cebee1c34f231659f8287423
adomani <adomani@gmail.com>
2024-12-12 20:13:02 +0100

    clean up and docs

--- Advents/AoC2024/day12.lean
+++ Advents/AoC2024/day12.lean
@@ -46,20 +46,55 @@ MMMISSJEEE"
 /-- `atest3` is the test string for the problem, split into rows. -/
 def atest3 := (test3.splitOn "\n").toArray
 
+/-- `testEX` is the test string for the problem. -/
+def testEX := "EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"
+
+/-- `atestEX` is the test string for the problem, split into rows. -/
+def atestEX := (testEX.splitOn "\n").toArray
+
+/-- `testAB` is the test string for the problem. -/
+def testAB := "AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"
+
+/-- `atestAB` is the test string for the problem, split into rows. -/
+def atestAB := (testAB.splitOn "\n").toArray
+
+/--
+The main structure to create connected components.
+* `gr` is the underlying grid.
+* `growing` is the set of vertices that we have so far added to our "connected component".
+* `front` is the front of the expansion: at the next step, we add the newighbours of `front` to
+  `growing`.
+* `edges` is the set of entries of `growing` that have a neighbour not in `gr`.
+* `nbs` is the set of neighbours. By default, it is the 4 coordinate directions.
+
+-/
 structure OneComp where
+  /-- `gr` is the underlying grid. -/
   gr : Std.HashSet pos
+  /-- `growing` is the set of vertices that we have so far added to our "connected component". -/
   growing : Std.HashSet pos
+  /-- `front` is the front of the expansion: at the next step, we add the newighbours of `front` to
+  `growing`. -/
   front : Std.HashSet pos
+  /-- `edges` is the set of entries of `growing` that have a neighbour not in `gr`. -/
   edges : Std.HashSet pos := {}
+  /-- `nbs` is the set of neighbours. By default, it is the 4 coordinate directions. -/
   nbs : Std.HashSet pos := {(1, 0), (- 1, 0), (0, 1), (0, - 1)}
   deriving Inhabited
 
-/-
-instance : ToString OneComp where
-  toString g :=
-    drawSparse g.growing
+/--
+Creates a "live" `OneComp`: this is not yet a connected components, just something that can
+`growComp` to be one.
 -/
-
 def mkOneComp {α} [BEq α] (g : Std.HashMap pos α) (c : α) (st : pos) : OneComp :=
   let oneVar := g.filter (fun _pos val => val == c)
   let gr : Std.HashSet pos := oneVar.fold (init := {}) fun h p _ => h.insert p
@@ -69,6 +104,7 @@ def mkOneComp {α} [BEq α] (g : Std.HashMap pos α) (c : α) (st : pos) : OneCo
     dbg_trace "'{st}' does not belong to the grid!"
     {gr := gr, growing := {st}, front := {st}}
 
+/-- Expands `growing` by each possible neighbour of `front`, updating also `edges` as necessary. -/
 def grow (c : OneComp) : OneComp := Id.run do
   let mut newR := c.growing
   let mut edges := c.edges
@@ -84,18 +120,23 @@ def grow (c : OneComp) : OneComp := Id.run do
         newF := newF.insert newpos
   return {c with growing := newR, front := newF, edges := edges}
 
+/--
+Creates the connected component "encoded" by its input,
+by growing the set `growing` as far as possible.
+-/
 def growComp (h : OneComp) : OneComp := Id.run do
   let mut grown := h
   while ! grown.front.isEmpty do
     grown := grow grown
   return grown
 
-def connectedComponent (h : OneComp) : Std.HashSet pos :=
-  growComp h |>.growing
+/-- The area of the connected component. -/
+def area (h : OneComp) : Nat := h.growing.size
 
-def area (h : OneComp) : Nat :=
-  h.growing.size
-
+/--
+Finds the perimeter of a connected component by tallying, for each edge, the directions that
+leave the component.
+-/
 def perimeter (h : OneComp) : Nat := Id.run do
   let mut tot := 0
   for e in h.edges do
@@ -104,71 +145,10 @@ def perimeter (h : OneComp) : Nat := Id.run do
         tot := tot + 1
   return tot
 
-/-
- 12
- 4
- 14
- 10
- 13
- 11
- 1
- 13
- 14
- 5
- 3
--/
-
 /--
-info:
-Start (1, 1), value: R, area : 12
-Start (6, 4), value: I, area : 14
-Start (2, 6), value: C, area : 14
-Start (0, 8), value: F, area : 10
-Start (4, 0), value: V, area : 13
-Start (6, 6), value: J, area : 11
-Start (2, 6), value: C, area : 14
-Start (6, 9), value: E, area : 13
-Start (6, 4), value: I, area : 14
-Start (8, 0), value: M, area : 5
-Start (9, 4), value: S, area : 3
--/
-#guard_msgs in
-#eval do
-  let dat := atest3
-  let tot := loadGrid dat id
-  let plants := #['R', 'I', 'C', 'F', 'V', 'J', 'C', 'E', 'I', 'M', 'S']
-  for p in plants do
-    let gr := sparseGrid dat (· == p)
-    let st := gr.toArray.back!
-    let gr := mkOneComp tot p st
-    let comp := connectedComponent gr
-    IO.println s!"Start {st}, value: {p}, area : {comp.size}"
-
-#eval do
-  let dat := atest3
-  let tot := loadGrid dat id
-  let plants := #['R', 'I', 'C', 'F', 'V', 'J', 'C', 'E', 'I', 'M', 'S']
-  for p in plants do
-    let gr := sparseGrid dat (· == p)
-    let st := gr.toArray.back!
-    let gr := mkOneComp tot p st
-    let compInfo := growComp gr
-    let perim := perimeter compInfo
-    let area := area compInfo
-    IO.println s!"Start {st}, value: {p}, area * perimeter: {area} * {perim} = {area * perim}"
-
-/-
-Start (7, 3), value: I, area : 14
-Start (9, 2), value: M, area : 5
-Start (3, 4), value: C, area : 14
-Start (3, 8), value: F, area : 10
-Start (5, 3), value: V, area : 13
-Start (9, 7), value: E, area : 13
-Start (0, 4), value: I, area : 4
-Start (1, 3), value: R, area : 12
-Start (8, 4), value: S, area : 3
-Start (6, 7), value: J, area : 11
-Start (4, 7), value: C, area : 1
+For each character that appears as a value in `tot`, determine the connected components of the
+set of positions with that character and return them all as an array,
+each with the corresponding character.
 -/
 def getComponents (tot : Std.HashMap pos Char) : Array (Char × OneComp) := Id.run do
   let mut tot := tot
@@ -181,9 +161,10 @@ def getComponents (tot : Std.HashMap pos Char) : Array (Char × OneComp) := Id.r
     fd := fd.push (p, comp)
   return fd
 
+/-- Computes the sum of `area * perimeter` over all the connected components in `tot`. -/
 def tallyAll (tot : Std.HashMap pos Char) : Nat :=
   let init := getComponents tot
-  init.foldl (fun t (_, h) => t + (h.growing.size * perimeter h)) 0 --· + ·.2.size)
+  init.foldl (fun t (_, h) => t + (h.growing.size * perimeter h)) 0
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := tallyAll <| loadGrid dat id
@@ -198,100 +179,11 @@ def part1 (dat : Array String) : Nat := tallyAll <| loadGrid dat id
 #  Question 2
 -/
 
-def rot (p : pos) : pos := (p.2, - p.1)
-
-def breaks (h : OneComp) : Nat := Id.run do
-  let mut tot := 0
-  for e in h.edges do
-    for n in h.nbs do
-      if (!h.growing.contains (e + n)) && ((!h.growing.contains ((e + n + rot n))) || (!h.growing.contains ((e + n - rot n)))) then
-        tot := tot + 1
-  return tot
-
-def breakAll (tot : Std.HashMap pos Char) : Nat :=
-  let init := getComponents tot
-  init.foldl (fun t (_, h) => t + (h.growing.size * breaks h)) 0 --· + ·.2.size)
-
---set_option trace.profiler true in
-#eval do
-  let dat ← IO.FS.lines input
-  let dat := atest3
-  let tot := loadGrid dat id
-  let init := getComponents tot
-  for (c, h) in init do
-    if c == 'R' then
-      IO.println <| s!"'{c}': area {area h}, breaks {breaks h}"
-
-/-!
--/
-
--- 1483212 -- 76s
 /--
-info:
-Value: I, area : 14
-Value: M, area : 5
-Value: C, area : 14
-Value: F, area : 10
-Value: V, area : 13
-Value: E, area : 13
-Value: I, area : 4
-Value: R, area : 12
-Value: S, area : 3
-Value: J, area : 11
-Value: C, area : 1
+Computes the `HashSet`s of the entries of `h` whose first coordinate is `i` and that do not have
+a left/right neighbour, storing them separately into the two output `HashSet`s.
 -/
-#guard_msgs in
-#eval do
-  let dat := atest3
-  let mut tot := loadGrid dat id
-  for (p, comp) in getComponents tot do
-    IO.println s!"Value: {p}, area : {area comp}"
-
-def corner (g : OneComp) (p : pos) : Nat :=
-  --let ld := #[(1, 0), (0, - 1)].map (g.growing.contains <| p + ·)
-  let angles := #[id, rot, rot ∘ rot, rot ∘ rot ∘ rot].map (#[(- 1, 0), (0, 1)].map ·)
-  let ru := (#[(- 1, 0), (0, 1)].map fun d => !g.growing.contains (p + d)).all id
-  let cond := angles.map fun dirs => (dirs.map fun d => !g.growing.contains (p + d)).all id
-  dbg_trace "{p}\nru: {ru}\ncond: {cond}\n"
-  default
-
-def dist (p q : pos) : Nat :=
-  let pq := p - q
-  (pq.1 ^ 2 + pq.2 ^ 2).natAbs
-
-def mkPath (front : Std.HashSet pos) : Array pos := Id.run do
-  let mut fd := #[]
-  let mut curr := front.toArray[0]!
-  let mut left := front.erase curr
-  while ! left.isEmpty do
-    let close := left.filter (dist · curr ≤ 2)
-    if close.isEmpty then return fd
-    let closer := close.filter (dist · curr ≤ 1)
-    curr := if closer.isEmpty then close.toArray[0]! else closer.toArray[0]!
-    fd := fd.push curr
-    left := left.erase curr
-  return fd
-
-instance : HMul Nat pos pos where hMul a p := (a * p.1, a * p.2)
-
-/-- Doubles and takes the Minkowski sum with the square of edge-length 2. -/
-def mmk' (h : Std.HashSet pos) : Std.HashSet pos :=
-  h.fold (fun new p => #[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)].foldl (·.insert <| 3 * p + ·) new) h
-
-/-- Doubles and takes the Minkowski sum with the square of edge-length 2. -/
-def mmk (h : Std.HashSet pos) : Std.HashSet pos :=
-  h.fold (fun new p => #[(0, 0), (0, 1), (1, 0), (1, 1)].foldl (·.insert <| 2 * p + ·) new) h
-
-def magnify (h : OneComp) : OneComp :=
-  {h with
-    gr := mmk h.gr
-    growing := mmk h.growing
-    front := mmk h.front
-    edges := mmk h.edges
-  }
-#check List.splitBy
---def lrBds (h : OneComp) (i : Int) : Std.HashMap pos Bool :=
-def lrBds (h : OneComp) (i : Int) : Std.HashSet Int × Std.HashSet Int :=
+def leftRightBounds (h : OneComp) (i : Int) : Std.HashSet Int × Std.HashSet Int :=
   h.edges.fold (fun new@(newl, newr) p =>
     if p.1 == i
     then
@@ -304,93 +196,33 @@ def lrBds (h : OneComp) (i : Int) : Std.HashSet Int × Std.HashSet Int :=
       new
     ) default
 
+/--
+Computes the corners in the component encoded in `h`, withing the bounds `m` and `M`.
+
+TODO: find the values of `m` and `M` and generally make this more efficients.
+-/
 def corners (h : OneComp) (m M : Nat) : Nat := Id.run do
   let mut diffs := 0
   let mut (cl, cr) : Std.HashSet Int × Std.HashSet Int := default
   for i in [m:M] do
-    let st1@(l, r) := lrBds h i
-    diffs := diffs + (cl.filter (!l.contains ·)).size + (l.filter (!cl.contains ·)).size
-    diffs := diffs + (cr.filter (!r.contains ·)).size + (r.filter (!cr.contains ·)).size
-    (cl, cr) := st1
+    let lr@(l, r) := leftRightBounds h i
+    diffs := diffs +
+      (cl.filter (!l.contains ·)).size + (l.filter (!cl.contains ·)).size +
+      (cr.filter (!r.contains ·)).size + (r.filter (!cr.contains ·)).size
+    (cl, cr) := lr
   return diffs
-set_option trace.profiler true in
-#eval do
-  let dat ← IO.FS.lines input
-  let _dat := atest3
-  let mut tot := loadGrid dat id
-  --let st : pos := (0, 0)
-  --let p := tot[st]!
-  let mut ult := 0
-  for (_p, comp) in getComponents tot do
-    ult := ult + area comp * corners comp 0 (dat.size + 1)
-    --IO.println s!"Value: {p}, area-corners: {area comp} * {corners comp 0 11} = {area comp * corners comp 0 11}"
-  IO.println s!"Final answer: {ult}"
-  --let comp := mkOneComp tot p st
-  --let comp := growComp comp
-  --IO.println <| corners comp 0 11
-  --let mut diffs := 0
-  --let mut (cl, cr) : Std.HashSet Int × Std.HashSet Int := default
-  --for i in [0:7] do
-  --  let st1@(l, r) := lrBds comp i
-  --  IO.println <| s!"{i}: {(l.toArray, r.toArray)}"
-  --  diffs := diffs + (cl.filter (!l.contains ·)).size + (l.filter (!cl.contains ·)).size
-  --  diffs := diffs + (cr.filter (!r.contains ·)).size + (r.filter (!cr.contains ·)).size
-  --  (cl, cr) := st1
-  --  IO.println diffs
-  --draw <| drawSparse comp.growing 15 20
-  --draw <| drawSparse comp.edges 15 20
-#exit
-  let mag' := magnify comp
-  let mag := growComp {mag' with front := mag'.edges, edges := {}}
-  let mag' := magnify mag
-  let mag := growComp {mag' with front := mag'.edges, edges := {}}
-  draw <| drawSparse mag.growing 50 80
-  draw <| drawSparse mag.edges 50 80
-  let mag' := magnify <| magnify comp
-  let mag := growComp {mag' with front := mag'.edges, edges := {}}
-  draw <| drawSparse mag.growing 80 80
-  draw <| drawSparse mag.edges 80 80
-  let path := mkPath mag.edges
-  draw <| drawSparse (.ofArray path) 80 80
-/-
-  let f := #[(0, 7), (1, 8), (5, 12), (8, 12)].map (corner comp)
-  dbg_trace f
-  let mut mk := comp.growing
-  for m in comp.growing do
-    for n in #[(1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)] do
-      mk := mk.insert (n + m)
-  draw <| drawSparse mk 20 20
-
-  IO.println s!"Value: {p}, area : {area comp}"
-  draw <| drawSparse comp.edges 20 20
-  draw <| drawSparse comp.growing 20 20
--/
-
-#eval do
-  let dat := atest2
-  let dat := atest1
-  let dat := atest3
-  let tot := loadGrid dat id
-  --let st : pos := (0, 0)
-  let plants : Std.HashSet Char := .ofArray tot.valuesArray
-  for p in plants do
-    let gr := sparseGrid dat (· == p)
-    let st := gr.toArray.back!
-    let gr := mkOneComp tot p st
-    --IO.println s!"initial grid, growing and grown for {p}"
-    --draw <| drawSparse gr.gr dat.size dat.size
-    --draw <| drawSparse gr.growing dat.size dat.size
-    let comp := connectedComponent gr
-    IO.println s!"Start {st}, value: {p}, area : {comp.size}"
-    draw <| drawSparse (connectedComponent gr) dat.size dat.size
-
 
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
-def part2 (dat : Array String) : Nat := sorry
---def part2 (dat : String) : Nat :=
+def part2 (dat : Array String) : Nat :=
+  let tot := loadGrid dat id
+  (getComponents tot).foldl (fun (_, comp) => · + area comp * corners comp 0 (dat.size + 1)) 0
 
---#assert part2 atest == ???
+#assert part2 atest1 == 80
+#assert part2 atest2 == 436
+#assert part2 atestEX == 236
+#assert part2 atestAB == 368
+#assert part2 atest3 == 1206
 
---solve 2
+--solve 2 897062
 
 end Day12
