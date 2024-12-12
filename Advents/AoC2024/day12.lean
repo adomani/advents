4a3e78e66b29a12bb97aa496fb86827660f49c2f
adomani <adomani@gmail.com>
2024-12-12 06:06:15 +0100

    tests

--- Advents/AoC2024/day12.lean
+++ Advents/AoC2024/day12.lean
@@ -12,14 +12,39 @@ def input : System.FilePath := "Advents/AoC2024/day12.input"
 
 --#eval do IO.println (← IO.FS.readFile input)
 
-/-- `test` is the test string for the problem. -/
-def test := "AAAA
+/-- `test1` is the test string for the problem. -/
+def test1 := "AAAA
 BBCD
 BBCC
 EEEC"
 
-/-- `atest` is the test string for the problem, split into rows. -/
-def atest := (test.splitOn "\n").toArray
+/-- `atest1` is the test string for the problem, split into rows. -/
+def atest1 := (test1.splitOn "\n").toArray
+
+/-- `test2` is the test string for the problem. -/
+def test2 := "OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"
+
+/-- `atest2` is the test string for the problem, split into rows. -/
+def atest2 := (test2.splitOn "\n").toArray
+
+/-- `test3` is the test string for the problem. -/
+def test3 := "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"
+
+/-- `atest3` is the test string for the problem, split into rows. -/
+def atest3 := (test3.splitOn "\n").toArray
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := sorry
