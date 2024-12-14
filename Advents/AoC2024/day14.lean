7f35923b38fc3fdcff34bd8ae9410944271bfea0
adomani <adomani@gmail.com>
2024-12-14 14:10:01 +0100

    docs and cleanup

--- Advents/AoC2024/day14.lean
+++ Advents/AoC2024/day14.lean
@@ -33,52 +33,59 @@ def size : Nat × Nat := (101, 103)
 /-- The size of the test area. -/
 def tsize : Nat × Nat := (11, 7)
 
+/--
+The main structure to encode the puzzle.
+* `ps` is the `HashSet` containing the position and velocity of each robot.
+* `sz` is the size of the grid.
+-/
 structure Robs where
+  /-- `ps` is the `HashSet` containing the position and velocity of each robot. -/
   ps : Std.HashSet (pos × pos)
+  /-- `sz` is the size of the grid. -/
   sz : Nat × Nat
 
+/-- Convert the input strings and a pair of sizes into a `Robs` structure. -/
 def inputToGrid (dat : Array String) (sz : Nat × Nat) : Robs where
   ps := dat.foldl (init := {}) fun h s => match s.getInts with
           | [p1, p2, v1, v2] => h.insert ((p2, p1), (v2, v1))
           | l => panic s!"There should be 4 integers in {l}"
   sz := (sz.2, sz.1)
 
+/-- A convenience function to multiply coordinatewise a position by an integer scalar. -/
 instance : HMul Int pos pos where hMul a p := (a * p.1, a * p.2)
 
+/-- A convenience function to add two vectors modulo the grid sizes. -/
 def add (r : Robs) (p v : pos) : pos :=
   let a := p + v
   (a.1 % r.sz.1, a.2 % r.sz.2)
 
+/-- Converts the input configuration `r` of robots into the configuration after `n` steps. -/
 def move (r : Robs) (n : Int) : Robs :=
   {r with ps := r.ps.fold (init := {}) fun h (p, v) => h.insert (add r p (n * v), v)}
 
-def tal (a b : Nat × Nat × Nat × Nat) : Nat × Nat × Nat × Nat :=
-  (a.1 + b.1, a.2.1 + b.2.1, a.2.2.1 + b.2.2.1, a.2.2.2 + b.2.2.2)
-
+/--
+Returns the quadruple of numbers of robots in each one of the four quadrants.
+The order of the quadrants is
+```
+(1 2)
+(3 4).
+```
+-/
 def counts (rs : Robs) : Nat × Nat × Nat × Nat :=
   let colSep := rs.sz.2 / 2
   let rowSep := rs.sz.1 / 2
   rs.ps.fold (init := (0, 0, 0, 0)) fun h (p, _v) =>
     h +
       if p.1 < rowSep && p.2 < colSep then (1, 0, 0, 0) else
-      if p.1 > rowSep && p.2 < colSep then (0, 1, 0, 0) else
-      if p.1 < rowSep && p.2 > colSep then (0, 0, 1, 0) else
+      if p.1 < rowSep && p.2 > colSep then (0, 1, 0, 0) else
+      if p.1 > rowSep && p.2 < colSep then (0, 0, 1, 0) else
       if p.1 > rowSep && p.2 > colSep then (0, 0, 0, 1) else
       (0, 0, 0, 0)
 
+/-- The safety measure is simply the product of the entries of the counts. -/
 def safety (rs : Robs) : Nat :=
   let (q1, q2, q3, q4) := counts rs
   q1 * q2 * q3 * q4
-
-#eval do
-  let (dat, sz) := (atest, tsize)
-  let (dat, sz) := (← IO.FS.lines input, size)
-  let rs := inputToGrid dat sz
-  --draw <| drawSparse (rs.ps.fold (fun h ((p, v) : pos × pos) => h.insert p) {}) (sz.2) (sz.1)
-  let r100 := move rs 100
-  --draw <| drawSparse (r100.ps.fold (fun h ((p, v) : pos × pos) => h.insert p) {}) (sz.2) (sz.1)
-  IO.println <| safety r100
-
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat :=
@@ -95,69 +102,6 @@ solve 1 220971520
 #  Question 2
 -/
 
-#eval do
-  let (dat, sz) := (atest, tsize)
-  let (dat, sz) := (← IO.FS.lines input, size)
-  let rs := inputToGrid dat sz
-  --draw <| drawSparse (rs.ps.fold (fun h ((p, v) : pos × pos) => h.insert p) {}) (sz.2) (sz.1)
-  let mut ccs : Std.HashMap _ Nat := {}
-  let bd := 100 --0 * sz.1 * sz.2
-  for i' in [0 * bd: bd] do
-    let i := 103 * i' + 72
-    let ri := move rs (i)
-    let cs@(c1, c2, c3, c4) := counts ri
-    ccs := ccs.alter cs fun v => some <| v.getD 0 + 1
-    IO.println i
-    draw <| drawSparse (ri.ps.fold (fun h ((p, v) : pos × pos) => h.insert p) {}) (sz.2) (sz.1)
-    if (c1 == c3 && c2 == c4) || (c1 == c2 && c3 == c4) then
-      --IO.println (i, cs)
-  let reps := ccs.filter fun ((c1, c2, c3, c4) : Nat × Nat × Nat × Nat) c => (c1 == c3 && c2 == c4) || (c1 == c2 && c3 == c4)
-  IO.println reps.size
-  IO.println reps.toArray
-
-#eval 101 * 103
-#eval 6355 % size.1
-
-
-#eval do
-  let (dat, sz) := (atest, tsize)
-  let (dat, sz) := (← IO.FS.lines input, size)
-  let rs := inputToGrid dat sz
-  --draw <| drawSparse (rs.ps.fold (fun h ((p, v) : pos × pos) => h.insert p) {}) (sz.2) (sz.1)
-  --let mut ccs : Std.HashMap _ Nat := {}
-  let bd := max sz.1 sz.2
-  let numRobs := rs.ps.size
-  let exp := (numRobs / 4) ^ 2 / 4
-  let mut results := #[]
-  for i' in [0:bd] do
-    let i := i'
-    let ri := move rs (i)
-    let (c1, c2, c3, c4) := counts ri
-    if c1 * c2 ≤ exp then results := results.push ((i, sz.1), 3, 4)
-    if c1 * c3 ≤ exp then results := results.push ((i, sz.2), 2, 4)
-    --if c1 * c4 ≤ exp then results := results.push ((i), 2, 3)
-    --if c2 * c3 ≤ exp then results := results.push ((i), 1, 4)
-    if c2 * c4 ≤ exp then results := results.push ((i, sz.2), 1, 3)
-    if c3 * c4 ≤ exp then results := results.push ((i, sz.1), 1, 2)
-    --ccs := ccs.alter cs fun v => some <| v.getD 0 + 1
-    --IO.println s!"{i}, {(numRobs / 4 ) ^ 4} {c1 * c2 * c3 * c4} -- {cs}"
-    --draw <| drawSparse (ri.ps.fold (fun h ((p, v) : pos × pos) => h.insert p) {}) (sz.2) (sz.1)
-    --if (c1 == c3 && c2 == c4) || (c1 == c2 && c3 == c4) then
-      --IO.println (i, cs)
-  match results with
-    | #[((val1, mod1), r1, r2), ((val2, mod2), s1, _s2)] =>
-      let corner := if r1 == s1 then r1 else r2
-      let ind := (Array.range mod2).filterMap fun i =>
-        if (val1 + i * mod1) % mod2 == val2 then some (val1 + i * mod1) else none
-      let xmas := drawSparse
-        ((move rs ind[0]!).ps.fold (fun h ((p, _) : pos × pos) => h.insert p) {}) sz.2 sz.1
-      dbg_trace String.intercalate "\n" <| s!"Look in quadrant number {corner}" :: xmas.toList
-      IO.println s!"{results}, corner: {corner}, index: {ind}"
-    | _ => IO.println "Modify the expected values!"
-  --let reps := ccs.filter fun ((c1, c2, c3, c4) : Nat × Nat × Nat × Nat) c => (c1 == c3 && c2 == c4) || (c1 == c2 && c3 == c4)
-  --IO.println reps.size
-  --IO.println reps.toArray
-
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (dat : Array String) : Nat :=
   let sz := if dat.size == atest.size then tsize else size
@@ -170,10 +114,10 @@ def part2 (dat : Array String) : Nat :=
   let results := (List.range bd).foldl (init := #[]) fun h (i : Nat) =>
     let ri := move rs i
     let (c1, c2, c3, c4) := counts ri
-    if c1 * c2 ≤ exp then h.push ((i, sz.1), 3, 4) else
-    if c1 * c3 ≤ exp then h.push ((i, sz.2), 2, 4) else
-    if c2 * c4 ≤ exp then h.push ((i, sz.2), 1, 3) else
-    if c3 * c4 ≤ exp then h.push ((i, sz.1), 1, 2) else
+    if c1 * c2 ≤ exp then h.push ((i, sz.2), 3, 4) else
+    if c1 * c3 ≤ exp then h.push ((i, sz.1), 2, 4) else
+    if c2 * c4 ≤ exp then h.push ((i, sz.1), 1, 3) else
+    if c3 * c4 ≤ exp then h.push ((i, sz.2), 1, 2) else
     h
   match results with
     | #[((val1, mod1), r1, r2), ((val2, mod2), s1, _s2)] =>
