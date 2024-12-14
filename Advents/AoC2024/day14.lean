ba8e9c636b12b735eb86a95b68d3f52640834e06
adomani <adomani@gmail.com>
2024-12-14 06:11:20 +0100

    tests

--- Advents/AoC2024/day14.lean
+++ Advents/AoC2024/day14.lean
@@ -13,7 +13,18 @@ def input : System.FilePath := "Advents/AoC2024/day14.input"
 --#eval do IO.println (← IO.FS.readFile input)
 
 /-- `test` is the test string for the problem. -/
-def test := ""
+def test := "p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"
 
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
