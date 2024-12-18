a8bc9edda12988c96ba7b04cb3c6c5f67c1339ef
adomani <adomani@gmail.com>
2024-12-18 13:19:31 +0100

    expand state to contain also corrupted, grow corrupted

--- Advents/AoC2024/day18.lean
+++ Advents/AoC2024/day18.lean
@@ -49,8 +49,11 @@ structure MS where
   f : Std.HashSet pos
   S : pos := (0, 0)
   tot : Nat := 0
-  front : Std.HashSet pos := {(0, 0)}
-  visited : Std.HashSet pos := {(0, 0)}
+  frontHistorians : Std.HashSet pos := {(0, 0)}
+  visitedHistorians : Std.HashSet pos := {(0, 0)}
+  frontCorrupted : Std.HashSet pos := f.filter fun (p, q) => p == 0 || q == exit.1
+  visitedCorrupted : Std.HashSet pos := f.filter fun (p, q) => p == 0 || q == exit.1
+
   sz : Nat
 
 def MS.available (ms : MS) (p : pos) : Bool :=
@@ -63,9 +66,9 @@ def inputToMS (s : Array String) (init sz : Nat) : MS :=
 
 def drawMS (ms : MS) : IO Unit := do
   IO.println s!"Steps: {ms.tot}"
-  draw <| drawSparseWith (ms.visited.union ms.f) (ms.sz + 1) (ms.sz + 1) (fun p =>
-    if ms.f.contains p && ms.visited.contains p then "*" else
-    if ms.f.contains p then "#" else if ms.visited.contains p then "O" else "·")
+  draw <| drawSparseWith (ms.visitedHistorians.union ms.f) (ms.sz + 1) (ms.sz + 1) (fun p =>
+    if ms.f.contains p && ms.visitedCorrupted.contains p then "*" else
+    if ms.f.contains p then "#" else if ms.visitedHistorians.contains p then "O" else "·")
 
 #eval do
   let (dat, sz, ex) := (atest, tone, texit)
@@ -75,23 +78,22 @@ def drawMS (ms : MS) : IO Unit := do
 abbrev nbs : Array pos := #[(0, 1), (0, - 1), (1, 0), (- 1, 0)]
 
 def move (ms : MS) : MS :=
-  let (newFront, newVisited) := ms.front.fold (init := (∅, ms.visited)) fun (hf, hv) p =>
+  let (newFront, newVisited) := ms.frontHistorians.fold (init := (∅, ms.visitedHistorians)) fun (hf, hv) p =>
     let new := nbs.filterMap fun n =>
       let pNew := p + n
       if ms.f.contains pNew || ! ms.available pNew then none else
       if !hv.contains pNew then some pNew else none
     (hf.insertMany new, hv.insertMany new)
-
   {ms with
-    front := newFront
-    visited := newVisited
+    frontHistorians := newFront
+    visitedHistorians := newVisited
     tot := ms.tot + 1 }
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) (one : Nat := one) : Nat := Id.run do
   let (sz, ex) := if dat.size ≤ 1000 then (tone, texit) else (one, exit)
   let mut ms := inputToMS dat sz ex.1
-  while (! ms.visited.contains ((ex.1, ex.2) : pos)) do
+  while (! ms.visitedHistorians.contains ((ex.1, ex.2) : pos)) do
     ms := move ms
   ms.tot
 
@@ -102,6 +104,44 @@ solve 1 380
 /-!
 #  Question 2
 -/
+
+abbrev blockNbs : Array pos := nbs ++ #[(1, 1), (1, - 1), (- 1, 1), (- 1, 1)]
+
+def moveBoth (ms : MS) : MS :=
+  -- expanding the accessible locations
+  let (newFrontH, newVisitedH) :=
+    ms.frontHistorians.fold (init := (∅, ms.visitedHistorians)) fun (hf, hv) p =>
+      let new := nbs.filterMap fun n =>
+        let pNew := p + n
+        if ms.f.contains pNew || ! ms.available pNew then none else
+        if !hv.contains pNew then some pNew else none
+    (hf.insertMany new, hv.insertMany new)
+  -- expanding the inaccessible locations
+  let (newFrontC, newVisitedC) :=
+    ms.frontCorrupted.fold (init := (∅, ms.visitedCorrupted)) fun (hf, hv) p =>
+      let new := blockNbs.filterMap fun n =>
+        let pNew := p + n
+        if (! ms.f.contains pNew) || hv.contains pNew then none else
+        some pNew
+    (hf.insertMany new, hv.insertMany new)
+  {ms with
+    frontHistorians := newFrontH
+    visitedHistorians := newVisitedH
+    frontCorrupted := newFrontC
+    visitedCorrupted := newVisitedC
+    tot := ms.tot + 1 }
+
+def blocker (ms : MS) : MS :=
+  let (newFront, newVisited) := ms.frontCorrupted.fold (init := (∅, ms.visitedCorrupted)) fun (hf, hv) p =>
+    let new := blockNbs.filterMap fun n =>
+      let pNew := p + n
+      if (! ms.f.contains pNew) || hv.contains pNew then none else
+      some pNew
+    (hf.insertMany new, hv.insertMany new)
+  {ms with
+    frontCorrupted := newFront
+    visitedCorrupted := newVisited
+    tot := ms.tot + 1 }
 
 #eval one / (2 ^ 9)
 
@@ -115,53 +155,91 @@ solve 1 380
   let mut ms := inputToMS dat sz ex.1
   --drawMS ms
   let mut vs := 0
-  while (! ms.visited.contains ((ex.1, ex.2) : pos)) || vs == ms.visited.size do
-    vs := ms.visited.size
-    ms := move ms
-  guard <| ms.visited.contains ((ex.1, ex.2) : pos)
+  while (! ms.visitedHistorians.contains ((ex.1, ex.2) : pos)) || vs == ms.visitedHistorians.size do
+    vs := ms.visitedHistorians.size
+    ms := moveBoth ms
+  guard <| ms.visitedHistorians.contains ((ex.1, ex.2) : pos)
 
   drawMS ms
   --IO.println ms.visited.toArray
 
-abbrev blockNbs : Array pos := nbs ++ #[(1, 1), (1, - 1), (- 1, 1), (- 1, 1)]
+def HorC (dat : Array String) (one : Nat) : Bool × pos := Id.run do
+    let ex := exit
+    let mut ms := inputToMS dat one ex.1
+    let last : pos := let cs := dat[one - 1]!.getNats; (cs[0]!, cs[1]!)
+    let newTarget : Std.HashSet pos :=
+      ms.f.filter fun (p, q) => q == 0 || p == exit.1
+      --(Std.HashSet.ofArray <| (Array.range (exit.1 - 1)).map (·.cast + 1, 0)).union
+      --  (.ofArray <| (Array.range exit.1).map (exit.1, ·.cast)) --== 0 || p == exit.1
 
-def blocker (ms : MS) : MS :=
-  let (newFront, newVisited) := ms.front.fold (init := (∅, ms.visited)) fun (hf, hv) p =>
-    let new := blockNbs.filterMap fun n =>
-      let pNew := p + n
-      if (! ms.f.contains pNew) || hv.contains pNew then none else
-      some pNew
-    (hf.insertMany new, hv.insertMany new)
-  {ms with
-    front := newFront
-    visited := newVisited
-    tot := ms.tot + 1 }
+    --IO.println <| s!"position: {one - 1}, last: {dat[one - 1]!}, {ms.f.contains last}, next: {dat[one]!}, {ms.f.contains next}"
+    --let newFront := ms.f.filter fun (p, q) => p == 0 || q == exit.1
+    --ms := {ms with frontCorrupted := newFront, visitedCorrupted := newFront}
+    --drawMS ms
+    --dbg_trace ((newTarget.filter ms.visitedCorrupted.contains).isEmpty,
+    --      (ms.visitedHistorians.contains ((exit.1, exit.2) : pos)))
+    while ((newTarget.filter ms.visitedCorrupted.contains).isEmpty &&
+          !(ms.visitedHistorians.contains ((exit.1, exit.2) : pos))) do
+      --vs := ms.visitedCorrupted.size
+      ms := moveBoth ms
+    --guard <| ms.visited.contains ((ex.1, ex.2) : pos)
+    return (ms.visitedHistorians.contains (exit.1, exit.2), last)
+    -- then IO.println "Historians" else IO.println "Corrupted"
 
-
+#exit
 #eval show Elab.Term.TermElabM _ from do
   --let dat := atest
   --let (sz, ex) := (tone, texit)
   let dat ← IO.FS.lines input
-  let one := (dat.size - 1) / 2 + (dat.size - 1) / 3 + (dat.size - 1) / 40 - 1
-  let pathExists := one - 1
-  let pathDoesNotExist := one
-  let (sz, ex) := (one, exit)
-  IO.println <| part1 dat pathExists
-  let mut ms := inputToMS dat sz ex.1
-  let last : pos := let cs := dat[one - 1]!.getNats; (cs[0]!, cs[1]!)
-  let next : pos := let cs := dat[one]!.getNats; (cs[0]!, cs[1]!)
-  IO.println <| s!"position: {one - 1}, last: {dat[one - 1]!}, {ms.f.contains last}, next: {dat[one]!}, {ms.f.contains next}"
-  let newFront := ms.f.filter fun (p, q) => p == 0 || q == exit.1
-  let newTarget := ms.f.filter fun (p, q) => q == 0 || p == exit.1
-  ms := {ms with front := newFront, visited := newFront}
-  drawMS ms
-  let mut vs := 0
-  while (newTarget.filter ms.visited.contains).isEmpty || vs == ms.visited.size do
-    vs := ms.visited.size
-    ms := blocker ms
-  --guard <| ms.visited.contains ((ex.1, ex.2) : pos)
+  --let one := (dat.size - 1) / 2 + (dat.size - 1) / 3 + (dat.size - 1) / 40 - 1
+  --let pathExists := one - 1
+  --let pathDoesNotExist := one
+  let ex := exit
+  --IO.println <| HorC dat 2958
+  --let (sz, ex) := (0, exit)
+  let mut ms : MS := {f := {}, sz := 0}
+    --(Std.HashSet.ofArray <| (Array.range (exit.1 - 1)).map (·.cast + 1, 0)).union
+    --  (.ofArray <| (Array.range exit.1).map (exit.1, ·.cast)) --== 0 || p == exit.1
+  --draw <| drawSparse newTarget (ex.1 + 1) (ex.1 + 1)
+--#exit
+  --ms := moveBoth ms
+  --drawMS ms
+  --if true then
+  let low := 2956
+  for one in [low:low + 3] do
+  --for one in [2955:2970] do
+    IO.println <| HorC dat one
+#exit
+    let mut con := 0
+    --ms := inputToMS dat one ex.1
+    --IO.println <| part1 dat one --pathExists
+    ms := inputToMS dat one ex.1
+    let newTarget : Std.HashSet pos :=
+      ms.f.filter fun (p, q) => q == 0 || p == exit.1
+    let last : pos := let cs := dat[one - 1]!.getNats; (cs[0]!, cs[1]!)
+    let next : pos := let cs := dat[one]!.getNats; (cs[0]!, cs[1]!)
+    --IO.println <| s!"position: {one - 1}, last: {dat[one - 1]!}, {ms.f.contains last}, next: {dat[one]!}, {ms.f.contains next}"
+    --let newFront := ms.f.filter fun (p, q) => p == 0 || q == exit.1
+    --ms := {ms with frontCorrupted := newFront, visitedCorrupted := newFront}
+    --drawMS ms
+    let mut vs := 0
+    --dbg_trace ((newTarget.filter ms.visitedCorrupted.contains).isEmpty,
+    --      (ms.visitedHistorians.contains ((exit.1, exit.2) : pos)))
+    while ((newTarget.filter ms.visitedCorrupted.contains).isEmpty &&
+          !(ms.visitedHistorians.contains ((exit.1, exit.2) : pos)) --do
+          --||
+          --vs == ms.visitedCorrupted.size
+          ) do --&&
+            --con ≤ 5000 do
+      --vs := ms.visitedCorrupted.size
+      ms := moveBoth ms
+      con := con + 1
+    --guard <| ms.visited.contains ((ex.1, ex.2) : pos)
+    IO.print s!"{con} "
+    if ms.visitedHistorians.contains (exit.1, exit.2) then IO.println "Historians" else IO.println "Corrupted"
+    --if ! (newTarget.filter ms.visitedCorrupted.contains).isEmpty then IO.println "Corrupted" else IO.println "Historians"
+    --drawMS ms
 
-  drawMS ms
 #exit
 
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
