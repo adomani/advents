031412311201ce911f88dc25c08c58a905a82cf3
adomani <adomani@gmail.com>
2024-12-22 08:19:05 +0100

    part 2

--- Advents/AoC2024/day22.lean
+++ Advents/AoC2024/day22.lean
@@ -51,7 +51,7 @@ def prune (s : Nat) := s % 16777216
 
 #assert prune 100000000 == 16113920
 
-def s1 (s : Nat) := prune <| mix (64 * s) s
+def s1 (s : Nat) := prune <| mix (s * 64) s
 def s2 (s : Nat) := prune <| mix (s / 32) s
 def s3 (s : Nat) := prune <| mix (s * 2048) s
 
@@ -90,10 +90,122 @@ set_option trace.profiler true in solve 1 14869099597 file
 #  Question 2
 -/
 
-#eval show Elab.Term.TermElabM _ from do
-  let t := test
+def price (s : Nat) := step s % 10
+
+def window (a : Array Int) (n : Int) : Array Int :=
+  if a.size < 4 then a.push n else a.reverse.pop.reverse.push n
+
+def changes (s : Nat) (it : Nat := 2000) (seq : Array Int := #[-2,1,-1,3]) : Array Int := Id.run do
+  let mut prevSecret := s
+  let mut tot : Array Int := #[]
+  let mut w : Array Int := #[]
+  for i in [1:it] do
+    let nextSecret := step prevSecret
+    let nextPrice : Int := nextSecret % 10
+    let change : Int := nextPrice - prevSecret % 10
+    w := window w change
+    --dbg_trace w
+    tot := tot.push <| change
+    prevSecret := nextSecret
+
+    if w == seq then
+      dbg_trace s!"Found at {i} with price {nextPrice}"
+      return #[nextPrice]
+  return tot
+
+
+#eval do
   let t ← IO.FS.readFile input
+  let t := test0
+  let t := "1,2,3,2024"
   let dat := t.getNats
+  let mut tot := 0
+  for d in dat do
+    let ch := changes d
+    match ch with
+      | #[p] => tot := tot + p
+      | _ => IO.println "Not found"
+  IO.println tot
+
+  --IO.println <| changes 123
+
+#eval Nat.factors 16777216
+#assert 2 ^ 24 == 16777216
+
+#eval do
+  let t ← IO.FS.readFile input
+  let t := test0
+  let dat := t.getNats
+  IO.println <| s!"{123}: {Nat.toDigits 2 (iter 123 2000)}"
+
+  for d in dat do
+    IO.println <| s!"{d}: {Nat.toDigits 2 d}"
+  let vals := dat.foldl (init := #[]) fun (m : Array Nat) d => m.push (iter d 2000)
+  IO.println vals.sum
+
+---def storeWindows (s : Array Int) : Std.HashSet (Array Int) :=
+---  s.foldl (init := ∅) fun h
+
+def storeWindows (h : Std.HashMap (Array Int) Int) (s : Nat) (it : Nat := 2000) :
+    Std.HashMap (Array Int) Int := Id.run do
+  let mut prevSecret := s
+  let mut alreadyFound : Std.HashSet (Array Int) := ∅
+  let mut tot := h
+  let mut w : Array Int := #[]
+  for i in [1:it+1] do
+    let nextSecret := step prevSecret
+    let nextPrice : Int := nextSecret % 10
+    let change : Int := nextPrice - prevSecret % 10
+    w := window w change
+    if w.size == 4 then
+      if ! alreadyFound.contains w then
+        tot := tot.alter w (some <| ·.getD 0 + nextPrice)
+        alreadyFound := alreadyFound.insert w
+    --dbg_trace w
+    prevSecret := nextSecret
+  return tot
+
+-- store the windows
+set_option trace.profiler true in
+#eval do
+  let dat := #[1, 2, 3, 2024]
+  let dat := (← IO.FS.readFile input).getNats.toArray
+  let mut tallies : Std.HashMap (Array Int) Int := ∅
+  for d in dat do
+    tallies := storeWindows tallies d
+  let mut (window, max) := (#[], 0)
+  for r@(w, t) in tallies do
+    if max < t then
+      IO.println s!"{w}: {t}"
+      (window, max) := r
+
+/-!
+-/
+#eval do
+  let mut n := 1
+  for i in [0:20] do
+    IO.println <| Nat.toDigits 2 <| 2 ^ 24 + n
+    n := step <| step <| step <| step n
+
+-- 1712 -- too low
+-- 1717 -- 0:it -- correct!
+-- 1717 -- 1:it+1 -- correct!
+/-
+#[-3, -1, -3, 6]: 755
+#[-3, 3, 0, 0]: 1315
+#[2, 0, 0, 3]: 1396
+#[3, 1, -1, 1]: 1474
+#[2, -2, 3, 1]: 1595
+#[0, -1, -3, 4]: 1605
+#[-1, 1, -1, 1]: 1676
+#[-1, 0, 0, 2]: 1712
+-/
+#eval show Elab.Term.TermElabM _ from do
+  let t ← IO.FS.readFile input
+  let t := test0
+  let dat := t.getNats
+  for d in dat do
+    IO.println <| s!"{d}: {Nat.toDigits 2 d}"
   let vals := dat.foldl (init := #[]) fun m d => m.push (iter d 2000)
   IO.println vals.sum
 
