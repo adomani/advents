4b2c8b2c8087cac65a060c1ae876302f3eb3e41f
adomani <adomani@gmail.com>
2024-12-29 09:48:54 +0100

    nbs and enhance

--- Advents/AoC2021/day20.lean
+++ Advents/AoC2021/day20.lean
@@ -43,10 +43,53 @@ def showImage (i : Image) : IO Unit := do
   IO.println i.iea
 
 #eval do
-  let dat := test
   let dat ← IO.FS.readFile input
+  let dat := test
   let i := inputToImage dat
   showImage i
+
+def nbs : Array pos := (Array.range 9).foldl (fun h i => h.push (i.cast / 3 - 1, i.cast % 3 - 1)) ∅
+
+#eval nbs
+
+def checkNbs (i : Image) (p : pos) : Array Bool := nbs.foldl (·.push <| i.light.contains <| p + ·) ∅
+
+#assert checkNbs (inputToImage test) (2, 2) ==
+        #[false, false, false, true, false, false, false, true, false]
+
+def binToNat (as : Array Bool) : Nat :=
+  Array.sum <| as.reverse.zipWith (Array.range as.size) fun a n => 2 ^ n * if a then 1 else 0
+
+#assert binToNat #[false, false, false, true, false, false, false, true, false] == 34
+
+def newChar (i : Image) (p : pos) : Char :=
+  let newCount : Nat := binToNat <| checkNbs i p
+  i.iea.get ⟨newCount⟩
+
+def enhance (i : Image) : Image :=
+  {i with
+    light := Id.run do
+      let mut newImage : Std.HashSet pos := ∅
+      for p in i.light do
+        if newChar i p == '#' then
+          newImage := newImage.insert p
+      return newImage}
+
+
+#eval do
+  let dat ← IO.FS.readFile input
+  let dat := test
+  let i := inputToImage dat
+  let i2 := enhance (enhance i)
+  --let mut newImage : Std.HashSet pos := ∅
+  --for p in i.light do
+  --  if newChar i p == '#' then newImage := newImage.insert p
+  IO.println i2.light.size
+  IO.println i2.light.toArray
+
+  showImage i2
+
+
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := sorry
