6f0c5ca35a6458c35999e39d86a93fa54b9c3f7a
adomani <adomani@gmail.com>
2025-01-04 09:11:15 +0100

    part 1

--- Advents/AoC2021/day25.lean
+++ Advents/AoC2021/day25.lean
@@ -1,5 +1,5 @@
 import Advents.Utils
-open Lean
+open Std
 
 namespace Day25
 
@@ -23,6 +23,81 @@ v.v..>>v.v
 
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
+
+structure SC where
+  grid : HashSet pos
+  cc : HashMap pos pos
+  w : Nat
+  h : Nat
+
+def inputToSC (dat : Array String) : SC where
+  grid := sparseGrid dat fun _ => true
+  cc := sparseMap dat fun c => match c with | '>' => some (0, 1) | 'v' => some (1, 0) | _ => none
+  w := dat[0]!.length
+  h := dat.size
+
+instance : ToString pos where toString | (0, 1) => ">"  | (1, 0) => "v" | _ => "."
+
+def drawSC (sc : SC) : IO Unit := draw <| drawHash sc.cc sc.h sc.w
+
+#eval do
+  let dat := atest
+  let sc := inputToSC dat
+  drawSC sc
+
+def add (sc : SC) (p q : pos) : pos :=
+  let cand := p + q
+  if sc.cc.contains cand then cand else
+  (cand.1 % sc.h, cand.2 % sc.w)
+
+def move (sc : SC) (d : pos) : SC := Id.run do
+  let mut (toMove, newCC) := sc.cc.partition fun _ p => p == d
+  for (p, m) in toMove do
+    let mv := add sc p m
+    if sc.cc.contains mv then
+      newCC := newCC.insert p m
+      --toMove := toMove.erase p
+    else
+      newCC := newCC.insert mv m
+  return {sc with cc := newCC}
+
+def flip (p : pos) : pos := (p.2, p.1)
+
+def moveN (sc : SC) (n : Nat) (d : pos := (0, 1)) : SC :=
+  match n with
+    | 0 => sc
+    | n + 1 => moveN (move sc d) n (flip d)
+
+def step (sc : SC) (n : Nat) : SC :=
+  match n with
+    | 0 => sc
+    | n + 1 => step (moveN sc 2) n
+
+#eval do
+  let dat := atest
+  let sc := inputToSC dat
+  drawSC sc
+  drawSC (move sc (0, 1))
+  drawSC (move (move sc (0, 1)) (1, 0))
+  --let sc := moveN sc 158 --(0, 1)
+  let sc := step sc 58
+  drawSC sc
+
+set_option trace.profiler true in
+#eval do
+  let dat := atest
+  let dat ← IO.FS.lines input
+  let mut sc := inputToSC dat
+  let mut con := 0
+  while true do
+    let scn := step sc 1
+    con := con + 1
+    if scn.cc.toArray.qsort (·.1 < ·.1) == sc.cc.toArray.qsort (·.1 < ·.1) then
+      IO.println con
+      break
+    sc := scn
+  drawSC sc
+
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := sorry
