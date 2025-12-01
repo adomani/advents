186d0a07f4327454a4f05cec4cce8372ef483bf7
Damiano Testa <adomani@gmail.com>
2025-12-01 09:45:24 +0000

    some changes

--- Advents/AoC2024/day07.lean
+++ Advents/AoC2024/day07.lean
@@ -29,21 +29,35 @@ to each element of `h` and the new element `n`.
 Since in our situation all operations are increasing, we only extend the `HashSet` if we obtain
 a value that does not exceed the maximum target `t`.
 -/
-def totalsWithOpsOne (t : Nat) (h : Std.HashSet Nat) (n : Nat) (ops : Array (Nat → Nat → Nat)) :
-    Std.HashSet Nat := Id.run do
+def totalsWithOpsOne (t : Nat) (h : Std.HashSet Nat × Std.HashSet Nat) (n : Nat) (ops : Array (Nat → Nat → Nat)) :
+    Std.HashSet Nat × Std.HashSet Nat := Id.run do
   let mut j := {}
-  for q in h do
+  let mut k := {}
+  for q in h.1 do
     for o in ops do
       let oqn := o q n
-      if oqn ≤ t then j := j.insert oqn
-  return j
+      if oqn ≤ t then j := j.insert oqn; k := k.insert (oqn % 9)
+  return (j, k)
+
+/--
+The concatenation operation on decimal digits of natural numbers.
+
+For instance, `cat 12 345 = 12345`.
+-/
+def cat (n m : Nat) : Nat := n * 10 ^ (Nat.toDigits 10 m).length + m
 
 /--
 Scan the entries of `ns`, accumulating the applications of the operations in `ops`,
 making sure that the result does not exceed `t`.
 -/
-def totalsWithOps (t : Nat) (ns : List Nat) (ops : Array (Nat → Nat → Nat)) : Std.HashSet Nat :=
-  (ns.drop 1).foldl (totalsWithOpsOne t · · ops) {ns[0]!}
+def totalsWithOps (t : Nat) (ns : List Nat) (ops : Array (Nat → Nat → Nat)) : Std.HashSet Nat × Std.HashSet Nat :=
+  --let withProds := (List.range ns.length).foldl (init := []) fun ps i => ps ++ [(ns.drop i).foldl (init := 0) cat]
+  --dbg_trace (ns.zip withProds)
+  --((ns.zip withProds).drop 1).foldl (fun h (n, res) =>
+  (ns.drop 1).foldl (fun h n =>
+    --let hmax := h.fold (init := 0) max
+    --if cat hmax (res) < t then dbg_trace "opt"; h else
+    totalsWithOpsOne t h n ops) ({ns[0]!}, {ns[0]! % 9})
 
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat :=
@@ -52,7 +66,7 @@ def part1 (dat : Array String) : Nat :=
     match ns with
       | [] => M
       | tot::ns =>
-        if (totalsWithOps tot ns #[(· * ·), (· + ·)]).contains tot
+        if (totalsWithOps tot ns #[(· * ·), (· + ·)]).1.contains tot
         then M + tot else M
 
 #assert part1 atest == 3749
@@ -63,13 +77,6 @@ solve 1 5540634308362
 #  Question 2
 -/
 
-/--
-The concatenation operation on decimal digits of natural numbers.
-
-For instance, `cat 12 345 = 12345`.
--/
-def cat (n m : Nat) : Nat := n * 10 ^ (Nat.toDigits 10 m).length + m
-
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (dat : Array String) : Nat :=
   let data := dat.map (·.getNats)
@@ -78,15 +85,21 @@ def part2 (dat : Array String) : Nat :=
       | [] => M
       | tot::ns =>
         -- this early test speeds up overall, even accounting for the repeated computations
-        if (totalsWithOps tot ns #[(· * ·), (· + ·)]).contains tot
-        then M + tot
-        else
-        if (totalsWithOps tot ns #[cat, (· * ·), (· + ·)]).contains tot
-        then M + tot
+        let (totAddMul, mod9) := (totalsWithOps tot ns #[(· * ·), (· + ·)])
+        if !mod9.contains (tot % 9) then dbg_trace "save {tot}"; M else
+        if (totalsWithOps tot ns #[cat, (· * ·), (· + ·)]).1.contains tot
+        then
+          M + tot
+        --else
+        --if (totAddMul.fold (init := (∅ : Std.HashSet Nat)) (·.insert <| · % 9)).contains (tot % 9) then
+        --  if (totalsWithOps tot ns #[cat, (· * ·), (· + ·)]).1.contains tot
+        --  then M + tot
+        --  else M
         else M
 
 #assert part2 atest == 11387
 
---set_option trace.profiler true in solve 2 472290821152397 -- takes approximately 1m30s
+
+set_option trace.profiler true in solve 2 472290821152397 -- takes approximately 1m30s
 
 end Day07
