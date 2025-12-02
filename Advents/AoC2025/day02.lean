5c49961df894dbe7d38af88bbbe46aa6efbc3097
adomani <adomani@gmail.com>
2025-12-02 05:02:12 +0000

    add data

--- Advents/AoC2025/day02.lean
+++ Advents/AoC2025/day02.lean
@@ -12,7 +12,9 @@ def input : FilePath := ("Advents"/"AoC2025"/"day02" : FilePath).withExtension "
 -/
 
 /-- `test` is the test string for the problem. -/
-def test := ""
+def test := "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124"
 
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
