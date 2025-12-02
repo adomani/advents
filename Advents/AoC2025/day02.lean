46edc56780199699ff124bab1b8fb899970139e6
adomani <adomani@gmail.com>
2025-12-02 06:07:57 +0000

    compute some ranges

--- Advents/AoC2025/day02.lean
+++ Advents/AoC2025/day02.lean
@@ -12,9 +12,47 @@ def input : FilePath := ("Advents"/"AoC2025"/"day02" : FilePath).withExtension "
 -/
 
 /-- `test` is the test string for the problem. -/
-def test := "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124"
+def test := "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+def inputToRanges (s : String) : Array (Nat × Nat) :=
+  let pairsStr := s.splitOn ","
+  let pairs := pairsStr.filterMap fun t =>
+    match t.getNats with
+    | [a, b] => some (a, b)
+    | _       => none
+  pairs.toArray
+
+def countResidues (res fin : Nat) (mod : Nat) : Nat :=
+  let res := res % mod
+  let guess := fin / mod
+  let correction := if res < (fin % mod) then 1 else 0
+  guess + correction
+
+-- Check that the pairs do not span ranges that are too big.
+#eval do
+  for dat in [test, ← IO.FS.readFile input] do
+    let pairs := inputToRanges dat
+    let twoOrdersOfMagnitude := pairs.filterMap fun ((a, b) : Nat × Nat) =>
+      if (Nat.toDigits 10 a).length + 2 ≤ (Nat.toDigits 10 b).length then some (a, b) else none
+    if !twoOrdersOfMagnitude.isEmpty then
+      IO.println "No pairs with at least two orders of magnitude difference."
+
+def toBase (a : Nat) : Nat :=
+  if a % 2 == 0 then
+    10 ^ ((Nat.toDigits 10 a).length / 2) + 1
+  else
+    10 ^ (((Nat.toDigits 10 a).length + 1) / 2) + 1
+
+-- #[2, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0]
+#eval do
+  let dat ← IO.FS.readFile input
+  let dat := test
+  let pairs := inputToRanges dat
+  let twoOrdersOfMagnitude := pairs.map fun ((a, b) : Nat × Nat) =>
+    let base := toBase b
+    countResidues (base - (a % base)) (b - a + 1) base
+    --if (Nat.toDigits 10 a).length + 2 ≤ (Nat.toDigits 10 b).length then some (a, b) else none
+  dbg_trace twoOrdersOfMagnitude
 
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
