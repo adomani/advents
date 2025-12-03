f7d209f72cf993e4de8e1163b22586c7b088c31e
adomani <adomani@gmail.com>
2025-12-03 05:18:47 +0000

    part1

--- Advents/AoC2025/day03.lean
+++ Advents/AoC2025/day03.lean
@@ -20,6 +20,31 @@ def test := "987654321111111
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
+def inputToDigits (s : String) : List Nat :=
+  s.toList.map fun c => ("".push c).toNat!
+
+def getMaxs (dat : List Nat) : Nat × Nat :=
+  let m1 := dat.max?.getD 0
+  let i1 := dat.findIdx (· == m1)
+  if i1 < dat.length - 1 then
+    let l2 := dat.drop (i1 + 1)
+    (m1, l2.max?.getD 0)
+  else
+  let dat2 := dat.erase m1
+  let m2 := dat2.max?.getD 0
+  let i2 := dat2.findIdx (· == m2)
+  if i1 ≤ i2 then
+    (m1, m2)
+  else
+    (m2, m1)
+
+#eval do
+  let dat := atest
+  let dat ← IO.FS.lines input
+  let digs := dat.map inputToDigits
+  let maxs := digs.map getMaxs
+  dbg_trace maxs.foldl (init := 0) fun tot (a, b) => tot + 10 * a + b
+
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := sorry
 --def part1 (dat : String) : Nat := sorry
