322a1b385940a2bab863753448a9b00f8b091489
Damiano Testa <adomani@gmail.com>
2025-12-07 06:52:29 +0100

    part 1

--- Advents/AoC2025/day07.lean
+++ Advents/AoC2025/day07.lean
@@ -32,6 +32,33 @@ def test := ".......S.......
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
+def inputToPos (dat : Array String) : HashSet pos :=
+  sparseGrid dat (· == '^')
+
+#eval do
+  let dat := atest
+  let dat ← IO.FS.lines input
+  --let sp := inputToPos dat
+  --draw <| drawSparse sp dat[0]!.length dat.size
+  let spos := dat[0]!.length - (dat[0]!.dropWhile (· != 'S')).length
+  dbg_trace "'S' is at position {spos}"
+  let mut tacs : HashSet Nat := {spos}
+  let mut splits := 0
+  for d in dat do
+    --dbg_trace tacs.toArray.qsort
+    let inds := sparseGrid (d.toList.toArray.map ("".push)) (· == '^')
+    if false && !inds.isEmpty then dbg_trace
+      s!"{tacs.toArray.qsort}\n{(inds.fold (·.push ·.1 : Array Int → Int × Int → Array Int) #[]).qsort} ← splitters"
+    (tacs, splits) := (Array.range dat[0]!.length).foldl (init := (∅, splits)) fun (tot, ss) n =>
+      if tacs.contains n then
+        if inds.contains (n, 0) then
+
+          (tot.insertMany [n - 1, n + 1], ss + 1)
+        else (tot.insert n, ss)
+      else (tot, ss)
+  dbg_trace "\n{splits} {tacs.size}"
+  dbg_trace "\n{tacs.toArray.qsort}"
+
 /-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
 def part1 (dat : Array String) : Nat := sorry
 --def part1 (dat : String) : Nat := sorry
