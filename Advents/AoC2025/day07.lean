ca6d78c9f26b2eb479e40d54eac7f2973547b2d7
Damiano Testa <adomani@gmail.com>
2025-12-07 07:18:22 +0100

    part 1 cleanup

--- Advents/AoC2025/day07.lean
+++ Advents/AoC2025/day07.lean
@@ -32,44 +32,57 @@ def test := ".......S.......
 /-- `atest` is the test string for the problem, split into rows. -/
 def atest := (test.splitOn "\n").toArray
 
-def inputToPos (dat : Array String) : HashSet pos :=
-  sparseGrid dat (· == '^')
+/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
+def part1 (dat : Array String) : Nat := Id.run do
+  let spos := dat[0]!.length - (dat[0]!.dropWhile (· != 'S')).length
+  let mut (tacs, splits) : HashSet Nat × Nat:= ({spos}, 0)
+  for d in dat do
+    let inds := sparseGrid (d.toList.toArray.map ("".push)) (· == '^')
+    if inds.isEmpty then continue
+    (tacs, splits) := (Array.range dat[0]!.length).foldl (init := (∅, splits))
+      fun (tot, ss) n =>
+        if tacs.contains n then
+          if inds.contains (n, 0) then
+            (tot.insertMany [n - 1, n + 1], ss + 1)
+          else (tot.insert n, ss)
+        else (tot, ss)
+  splits
+
+#assert part1 atest == 21
+
+solve 1 1553
+
+/-!
+#  Question 2
+-/
 
 #eval do
-  let dat := atest
   let dat ← IO.FS.lines input
+  let dat := atest
   --let sp := inputToPos dat
   --draw <| drawSparse sp dat[0]!.length dat.size
   let spos := dat[0]!.length - (dat[0]!.dropWhile (· != 'S')).length
   dbg_trace "'S' is at position {spos}"
   let mut tacs : HashSet Nat := {spos}
-  let mut splits := 0
+  let mut splits := 1
   for d in dat do
     --dbg_trace tacs.toArray.qsort
     let inds := sparseGrid (d.toList.toArray.map ("".push)) (· == '^')
-    if false && !inds.isEmpty then dbg_trace
-      s!"{tacs.toArray.qsort}\n{(inds.fold (·.push ·.1 : Array Int → Int × Int → Array Int) #[]).qsort} ← splitters"
-    (tacs, splits) := (Array.range dat[0]!.length).foldl (init := (∅, splits)) fun (tot, ss) n =>
-      if tacs.contains n then
-        if inds.contains (n, 0) then
-
-          (tot.insertMany [n - 1, n + 1], ss + 1)
-        else (tot.insert n, ss)
-      else (tot, ss)
+    if inds.isEmpty then continue
+    let (newtacs, newsplits) := (Array.range dat[0]!.length).foldl (init := (∅, 0))
+      fun ((tot : HashSet Nat), ss) n =>
+        if tacs.contains n then
+          if inds.contains (n, 0) then
+            (tot.insertMany [n - 1, n + 1], ss * 2)
+          else (tot.insert n, ss)
+        else (tot, ss)
+    if true then dbg_trace
+      s!"{(tacs.toArray.qsort)} {(splits, newsplits)}\n{(inds.fold (·.push ·.1 : Array Int → Int × Int → Array Int) #[]).qsort} ← splitters"
+    tacs := newtacs
+    splits := splits + tacs.size
   dbg_trace "\n{splits} {tacs.size}"
+  dbg_trace "\n{tacs.size}"
   dbg_trace "\n{tacs.toArray.qsort}"
-
-/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
-def part1 (dat : Array String) : Nat := sorry
---def part1 (dat : String) : Nat := sorry
-
---#assert part1 atest == ???
-
---set_option trace.profiler true in solve 1
-
-/-!
-#  Question 2
--/
 
 /-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
 def part2 (dat : Array String) : Nat := sorry
