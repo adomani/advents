30c615b8239f6fa17f0ad4eef981beaaee02e888
adomani <adomani@gmail.com>
2025-12-08 04:58:55 +0000

    first commit

---
+++ Advents/AoC2025/day08.lean
@@ -0,0 +1,59 @@
+import Advents.Utils
+open Std
+
+namespace AoC2025_Day08
+
+open System in
+/-- `input` is the location of the file with the data for the problem. -/
+def input : FilePath := ("Advents"/"AoC2025"/"day08" : FilePath).withExtension "input"
+
+/-!
+#  Question 1
+-/
+
+/-- `test` is the test string for the problem. -/
+def test := "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689"
+
+/-- `atest` is the test string for the problem, split into rows. -/
+def atest := (test.splitOn "\n").toArray
+
+/-- `part1 dat` takes as input the input of the problem and returns the solution to part 1. -/
+def part1 (dat : Array String) : Nat := sorry
+--def part1 (dat : String) : Nat := sorry
+
+--#assert part1 atest == ???
+
+--set_option trace.profiler true in solve 1
+
+/-!
+#  Question 2
+-/
+
+/-- `part2 dat` takes as input the input of the problem and returns the solution to part 2. -/
+def part2 (dat : Array String) : Nat := sorry
+--def part2 (dat : String) : Nat :=
+
+--#assert part2 atest == ???
+
+--set_option trace.profiler true in solve 2
+
+end AoC2025_Day08
