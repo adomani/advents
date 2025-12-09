c6df408e4ffa07744eafdb4c230c5c2aebd9aa1c
adomani <adomani@gmail.com>
2025-12-09 06:47:38 +0000

    tests

--- Advents/AoC2025/day09.lean
+++ Advents/AoC2025/day09.lean
@@ -48,23 +48,41 @@ set_option trace.profiler true in solve 1 4767418746
 #  Question 2
 -/
 
-#eval do
-  let dat := atest
-  let dat ← IO.FS.lines input
-  let gr := inputToPos dat
-  let (mx, Mx) : Option Int × Option Int := gr.fold (init := (none, none)) fun (mx, Mx) ((a, _) : pos) =>
-    match mx, Mx with
-    | some m, some M => (min a m, max a M)
-    | none, some M => (a, max a M)
-    | some m, none => (min a m, a)
-    | none, none => (a, a)
+def inputToArray (dat : Array String) : Array pos :=
+  dat.foldl (init := #[]) fun tot s => match s.getNats with | [x, y] => tot.push (x, y) | _ => tot
 
-  let (my, My) : Option Int × Option Int := gr.fold (init := (none, none)) fun (my, My) ((_, b) : pos) =>
-    match my, My with
-    | some m, some M => (min b m, max b M)
-    | none, some M => (b, max b M)
-    | some m, none => (min b m, b)
-    | none, none => (b, b)
+-- Check that the input is a snake
+#eval do
+  let dat ← IO.FS.lines input
+  let dat := atest
+  let gr := inputToArray dat
+  let switch := if dat.size ≤ 100 then 0 else 1
+  for i in [:gr.size] do
+    let (a, b) := gr[i]!
+    let (c, d) := gr[i + 1]?.getD gr[0]!
+    if i % 2 == switch && b != d then dbg_trace "Consecutive second coordinates do not match!"
+    if i % 2 != switch && a != c then dbg_trace "Consecutive first coordinates do not match!"
+  let (xs, ys) := gr.unzip
+  if (HashSet.ofArray xs).size * 2 != dat.size then dbg_trace "Coincidence among first coordinates!"
+  if (HashSet.ofArray ys).size * 2 != dat.size then dbg_trace "Coincidence among second coordinates!"
+
+#eval do
+  let dat ← IO.FS.lines input
+  let dat := atest
+  let gr := inputToArray dat
+  --let (mx, Mx) : Option Int × Option Int := gr.fold (init := (none, none)) fun (mx, Mx) ((a, _) : pos) =>
+  --  match mx, Mx with
+  --  | some m, some M => (min a m, max a M)
+  --  | none, some M => (a, max a M)
+  --  | some m, none => (min a m, a)
+  --  | none, none => (a, a)
+
+  --let (my, My) : Option Int × Option Int := gr.fold (init := (none, none)) fun (my, My) ((_, b) : pos) =>
+  --  match my, My with
+  --  | some m, some M => (min b m, max b M)
+  --  | none, some M => (b, max b M)
+  --  | some m, none => (min b m, b)
+  --  | none, none => (b, b)
 
   --let (mx, MY) : Option Int × Option Int := gr.fold (init := (none, none)) fun (mx, MY) ((a, b) : pos) =>
   --  match mx, MY with
@@ -80,6 +98,18 @@ set_option trace.profiler true in solve 1 4767418746
   let mut areas : HashSet Nat := ∅
   let mut maxArea := 0
   let mut left := gr
+  let switch := if dat.size ≤ 100 then 0 else 1
+  for i in [:gr.size] do
+    let (a, b) := gr[i]!
+    let (c, d) := gr[i + 1]?.getD gr[0]!
+    if i % 2 == switch && b != d then dbg_trace "Fail!"
+    if i % 2 != switch && a != c then dbg_trace "Fail!"
+    --if i % 2 != switch && a != c then dbg_trace "Fail!"
+    for (c, d) in left do
+      let newArea := ((a - c).natAbs + 1) * ((b - d).natAbs + 1)
+      maxArea := max maxArea newArea
+      areas := areas.insert newArea
+
   for A@(a, b) in gr do
     left := left.erase A
     for (c, d) in left do
